848453355999a30fd5f33488e5b12651f52b02b2
idembele70 <idembele70@gmail.com>
2021-02-08 20:57:25 +0100

    solutions

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,34 @@
 document.body.style.backgroundColor = "black";
 document.body.style.color = "white";
 document.body.innerHTML = "white";
-let string ='sak';
-let minVal, minMedVal, result;
-function sumTwoSmallestNumbers(numbers) {  
-    for (let i = 0; i <= numbers.length; i++) {
-        let numbersToInt = parseInt(numbers[i]);
-        if (Number.isInteger(numbersToInt) && numbersToInt >= 0) {
-            if (Number.isInteger(minVal)) {
-                minVal = Math.min(minVal, numbersToInt);
-            } else {
-                minVal = numbersToInt;
+let string = 'sak';
+
+function sumTwoSmallestNumbers(numbers = [1, 2, 4, 5]) {
+    let min = Number.MAX_SAFE_INTEGER,
+        secondMin = Number.MAX_SAFE_INTEGER;
+
+        let n;
+        for (let i = 0; i < numbers.length; i++) {
+            n = numbers[i];
+            if (n < min) {
+                secondMin = min;
+                min = n;
+            } else if (n < secondMin) {
+                secondMin = n;
             }
+            
         }
-    }
-   let  toDel = numbers.indexOf(minVal);
-    numbers.splice(toDel, 1);
-    for (let j = 0; j < numbers.length; j++) {
-        let numbersToInt = parseInt(numbers[j]);
-        if (Number.isInteger(numbersToInt) && numbersToInt >= 0) {
-            if (Number.isInteger(minMedVal)) {
-                minMedVal = Math.min(minMedVal, numbersToInt);
-            } else {
-                minMedVal = numbersToInt;
-            }
-        }
-    }
-    result = minVal + minMedVal;
-    return result;;
+        return min + secondMin;
 }
 
-// console.log(sumTwoSmallestNumbers([2, 270, 32, 1, 22]));
-console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]), 13 );
+
+// console.log(sumTwoSmallestNumbers([1, 7, 2,222, 112, 232]));
+
+
+
+console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]), 13);
 console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]), 6);
-console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7]), 10 );
+console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7]), 10);
 console.log(sumTwoSmallestNumbers([23, 71, 33, 82, 1]), 24);
 console.log(sumTwoSmallestNumbers([52, 76, 14, 12, 4]), 16);
 
