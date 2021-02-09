f6df2e95590afab02eb9ba874db008c1cd99f494
idembele70 <idembele70@gmail.com>
2021-02-09 21:51:24 +0100

    multiple of 3 or 5

--- src/js/main.js
+++ src/js/main.js
@@ -3,33 +3,15 @@ document.body.style.color = "white";
 document.body.innerHTML = "white";
 let string = 'sak';
 
-function sumTwoSmallestNumbers(numbers = [1, 2, 4, 5]) {
-    let min = Number.MAX_SAFE_INTEGER,
-        secondMin = Number.MAX_SAFE_INTEGER;
-
-        let n;
-        for (let i = 0; i < numbers.length; i++) {
-            n = numbers[i];
-            if (n < min) {
-                secondMin = min;
-                min = n;
-            } else if (n < secondMin) {
-                secondMin = n;
-            }
-            
-        }
-        return min + secondMin;
+function solution(number=10){
+    let resultat = 0 ;
+  for (let i = 1; i < number; i++) {
+      if (i % 3 == 0 || i % 5 == 0 ) {
+          resultat +=i;
+      }
+      
+  }
+  return resultat;
 }
 
-
-// console.log(sumTwoSmallestNumbers([1, 7, 2,222, 112, 232]));
-
-
-
-console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]), 13);
-console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]), 6);
-console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7]), 10);
-console.log(sumTwoSmallestNumbers([23, 71, 33, 82, 1]), 24);
-console.log(sumTwoSmallestNumbers([52, 76, 14, 12, 4]), 16);
-
-
+solution();
