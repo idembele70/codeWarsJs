b572af6bd760733c42ae9880813d16180ad87f56
idembele70 <idembele70@gmail.com>
2021-02-09 22:00:20 +0100

    add binary

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,9 @@ document.body.style.color = "white";
 document.body.innerHTML = "white";
 let string = 'sak';
 
-function solution(number=10){
-    let resultat = 0 ;
-  for (let i = 1; i < number; i++) {
-      if (i % 3 == 0 || i % 5 == 0 ) {
-          resultat +=i;
-      }
-      
-  }
-  return resultat;
+function addBinary(a=1,b=2) {
+const result = a + b;
+ return result.toString(2);
 }
 
-solution();
+addBinary();
