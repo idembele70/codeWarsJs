d1ee6edcf065fb2e7284517b9da277acd9559e71
idembele70 <idembele70@gmail.com>
2021-02-10 17:03:55 +0100

    isValidWalk

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,60 @@
 document.body.style.backgroundColor = "black";
 document.body.style.color = "white";
 document.body.innerHTML = "white";
-let str = 'a,lu,i,s'
-function isValidWalk(walk) {
-    const RemoweVirgule = new RegExp(',','g');
-  let  container = walk.toString();
+
+// function isValidWalk(walk=['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's']) {
+//     const RemoweVirgule = new RegExp(',','g');
+//   let  container = walk.toString();
+//     if (walk.length == 10) {
+//         for (let i = 0; i < container.length; i++) {
+//             if (container.charAt(i).match(RemoweVirgule)) {
+//                container =  container.replace(container.charAt(i),'');
+//                 container.charAt(i)
+//             }
+//         }
+//     console.log(container);   
+//      container = container.replaceAll(',','');
+//      console.log(container);
+//      const regexNs = new RegExp('ns','g');
+//      const regexWe = new RegExp('we','g');
+//         if (container.match(regexNs).length == 5 || container.match(regexWe).length == 5) {
+//             console.log(true);
+//         } else {
+//             console.log(false);
+//         }
+//     } else {
+//         console.log(false);
+//      }
+// }
+
+function isValidWalk(walk = ['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's']) {
+    const RemoweVirgule = new RegExp(',', 'g');
+    let container = walk.toString();
     if (walk.length == 10) {
         for (let i = 0; i < container.length; i++) {
             if (container.charAt(i).match(RemoweVirgule)) {
-               container =  container.replace(container.charAt(i),'');
+                container = container.replace(container.charAt(i), '');
                 container.charAt(i)
             }
-            
         }
-        console.log(container);   
-     container = container.replaceAll(',','');
-     console.log(container);
-     const MyRegex = new RegExp('ns','g');
-        if (container.match(MyRegex).leng == 5) {
-            console.log('ok');
+        const regexNs = new RegExp('ns', 'g');
+        const regexWe = new RegExp('we', 'g');
+        let nsLength, weLength;
+        if (container.match(regexNs)) {
+             nsLength = container.match(regexNs).length;
         } else {
-            console.log(false);
+             weLength = container.match(regexWe).length;
+        }
+        if (nsLength == 5 || weLength == 5) {
+            return true;
+        } else {
+            return false;
         }
     } else {
-        console.log(false);
-     }
+        return false;
+    }
 }
-isValidWalk(['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's']);
 
+console.log(isValidWalk(['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's']));
+console.log(!isValidWalk(['w','e','w','e','w','e','w','e','w','e']));
 
