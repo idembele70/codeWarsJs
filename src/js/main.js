8f42283082979d0a10de56bf33b7886ae99769b2
idembele70 <idembele70@gmail.com>
2021-02-12 15:57:48 +0100

    fini

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,16 @@
-document.body.style.backgroundColor = "black";
-document.body.style.color = "white";
-document.body.innerHTML = "white";
-let string = 'sak';
+// document.body.style.backgroundColor = "black";
+// document.body.style.color = "white";
+// document.body.innerHTML = "white";
+// let string = 'sak';
 
-function addBinary(a=1,b=2) {
-const result = a + b;
- return result.toString(2);
+function solution(number=10){
+    let result=0;
+    for (let i = 0; i < number; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            result +=i;
+        }
+    }
+    return result
 }
 
-addBinary();
+console.log(solution());
