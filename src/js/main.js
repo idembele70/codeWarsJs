6e0e26512326fd08362d7658e06de4a23fc27579
idembele70 <idembele70@gmail.com>
2021-02-15 02:31:13 +0100

    en cours

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,22 @@
-function filter_list(l=[1,2,'a','b']){
-   return l.filter(function (v) {
-       return typeof v !== 'string';
-   });
+function isPangram(string="The quick brown fox jumps over the lazy dog."){
+    string = string.toLowerCase();
+    string = string.split('').sort().toString('');
+    const regex=/[a-z]/gi;
+    const container = '';
+    for (let i = 0; i < string.length; i++) {
+        if (string.charAt(i)) {
+            for (let j = 0; j < container.length; j++) {
+                if (container.match()) {
+                } else {
+                    container.push(string.charAt(i));
+                    
+                }
+                
+            }
+        }
+        
+    }
+    return container.length;
   }
 
-  console.log(filter_list());
+  console.log(isPangram());
