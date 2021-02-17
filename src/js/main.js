6483f4c4ab1eba6c1cfdf9053b0d4e9875c7f104
idembele70 <idembele70@gmail.com>
2021-02-17 20:52:51 +0100

    fini

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,18 @@
-function isPangram(string="The quick brown fox jumps over the lazy dog."){
-   
-    return (string.match(/([a-z])(?!.*\1)/ig) || []).length === 26;
-   
+function digital_root(n = 493193) {
+  let resultat = 0, i = 0;
+  nLength = n.toString().length;
+  while (!!i <= nLength) {
+    n.toString().split('').forEach(element => {
+      resultat += parseInt(element);
+    });
+    n = resultat;
+    resultat = 0;
+    if (n.toString().length == 1) {
+      return n;
+    }
+    ++i;
   }
 
-  console.log(isPangram());
+}
 
-
+console.log(digital_root());
