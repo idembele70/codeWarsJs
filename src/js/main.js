818cee5410b2422f11cdc7a1477889de3c234484
idembele70 <idembele70@gmail.com>
2021-02-19 00:52:59 +0100

    preque fini

--- src/js/main.js
+++ src/js/main.js
@@ -1,61 +1,23 @@
-function solution(roman =21 ) {
-  let romanString = '';
-  while (roman > 0) {
-    if (1000 <= roman) {
-      let  Dlong = parseInt(roman / 1000);
-        roman = roman % 1000;
-        for (let i = 0; i < Dlong; i++) {
-          romanString += 'M';
-        }
-      }
-
-    if (500 <= roman < 1000) {
-      let  Dlong = parseInt(roman / 500);
-        roman = roman % 500;
-        for (let i = 0; i < Dlong; i++) {
-          romanString += 'D';
-        }
-      }
-    if (100 <= roman < 500) {
-      let  Clong = parseInt(roman / 100);
-        roman = roman % 100;
-        for (let i = 0; i < Clong; i++) {
-          romanString += 'C';
-        }
-      }
-
-    if (50 <= roman < 100) {
-      let  Llong = parseInt(roman / 50);
-        roman = roman % 50;
-        for (let i = 0; i < Llong; i++) {
-          romanString += 'L';
-        }
-      }
-
-    if (10 <= roman < 50) {
-    let  Mlong = parseInt(roman / 10);
-      roman = roman % 10;
-      for (let i = 0; i < Mlong; i++) {
-        romanString += 'X';
-      }
-    }
-     if (5 <= roman < 10) {
-     let Vlong = parseInt(roman / 5);
-      roman = roman % 5;
-      for (let i = 0; i < Vlong; i++) {
-        romanString += 'V';
-      }
-    }
-     if (1 <= roman < 5) {
-     let Ilong = parseInt(roman / 1);
-     roman = roman % 1;
-     for (let i = 0; i < Ilong; i++) {
-       romanString += 'I';
-      }
-      
-    }
-  }
-  return romanString;
+function solution(roman ='IV' ) {
+  let number = 0;
+  roman.split('').forEach(element => {
+    if (element === "M") {
+      number += 1000
+    } else if (element === "D") {
+      number +=500;
+    } else if (element === "C") {
+      number+= 100;
+    } else if (element === "L") {
+      number+= 50;
+    } else if (element === "X") {
+      number+= 10;
+    } else if (element === "V") {
+      number+= 5;
+    } else if (element === "I") {
+      number+= 1;
+    } 
+  })
+  return number;
 }
 
 console.log(solution());
