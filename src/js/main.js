afbfb237c11a566016f5e2ea3b21aa94d8eb418a
idembele70 <idembele70@gmail.com>
2021-02-19 21:55:23 +0100

    Ma version finie

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,44 @@
-function solution(roman = '') {
-  let number = 0; 
-  roman.split('').forEach(element => {
-    if (element === "M") {
-      number += 1000
-    } else if (element === "D") {
-      number +=500;
-    } else if (element === "C") {
-      number+= 100;
-    } else if (element === "L") {
-      number+= 50;
-    } else if (element === "X") {
-      number+= 10;
-    } else if (element === "V") {
-      number+= 5;
-    } else if (element === "I") {
-      number+= 1;
+function solution(roman = 'XXI') {
+  let number = 0;
+  roman = roman.split('')
+  for (let i = 0; i < roman.length; i++) {
+    if (roman[i] === "M") {
+      // number += 1000
+      roman[i] =1000;
+
+    } else if (roman[i] === "D") {
+      // number +=500;
+      roman[i] = 500;
+    } else if (roman[i] === "C") {
+      roman[i] = 100;
+    } else if (roman[i] === "L") {
+      // number+= 50;
+      roman[i] = 50;
+    } else if (roman[i] === "X") {
+      // number+= 10;
+      roman[i] =10;
+
+    } else if (roman[i] === "V") {
+      // number+= 5;
+      roman[i] =5;
+
     } 
-  })
+    else if (roman[i] === "I") {
+      // number+= 1;
+      roman[i] =1;
+
+    } 
+  }
+
+  for (let j = 0; j < roman.length; j++) {
+    if (roman[j] >= roman[roman.indexOf(roman[j])+1] || !Number.isInteger(roman[roman.indexOf(roman[j])+1]))  {
+      number += roman[j];
+    } else if(roman[j] < roman[roman.indexOf(roman[j])+1])  {
+      number -= roman[j]
+    } else {
+    }
+    
+  }
   return number;
 }
 
