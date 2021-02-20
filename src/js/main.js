8076a15956f69224a5659885d8767a1d940b7553
idembele70 <idembele70@gmail.com>
2021-02-20 22:55:08 +0100

    ARRAY PROTOTYPE

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,10 @@
-function solution(roman = 'XXI') {
-  let number = 0;
-  roman = roman.split('')
-  for (let i = 0; i < roman.length; i++) {
-    if (roman[i] === "M") {
-      // number += 1000
-      roman[i] =1000;
-
-    } else if (roman[i] === "D") {
-      // number +=500;
-      roman[i] = 500;
-    } else if (roman[i] === "C") {
-      roman[i] = 100;
-    } else if (roman[i] === "L") {
-      // number+= 50;
-      roman[i] = 50;
-    } else if (roman[i] === "X") {
-      // number+= 10;
-      roman[i] =10;
-
-    } else if (roman[i] === "V") {
-      // number+= 5;
-      roman[i] =5;
-
-    } 
-    else if (roman[i] === "I") {
-      // number+= 1;
-      roman[i] =1;
-
-    } 
+Array.prototype.filter = function (func) {
+  let res = [];
+  for (let i = 0; i < this.length; i++) {
+      if (func(this[i])) {
+          res.push(this[i])
+      }
   }
-
-  for (let j = 0; j < roman.length; j++) {
-    if (roman[j] >= roman[roman.indexOf(roman[j])+1] || !Number.isInteger(roman[roman.indexOf(roman[j])+1]))  {
-      number += roman[j];
-    } else if(roman[j] < roman[roman.indexOf(roman[j])+1])  {
-      number -= roman[j]
-    } else {
-    }
-    
-  }
-  return number;
+  return res;
 }
-
-console.log(solution());
+console.log([1,2,3,4].filter((num)=>{return num > 3}))
