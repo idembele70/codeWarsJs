c1c6e4585b75bf64a260f9ea711797ce9a010f0d
idembele70 <idembele70@gmail.com>
2021-02-28 12:52:34 +0100

    fini premier version

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,25 @@
 // complete the function
-function solution(string='camelCasingTCasing') {
-
-return  string.split(/([A-Z])/)
+function accum(s='abcd') {
+    const tabs = []; let compteur = -1;
+	 s.split('').forEach((element)=>{
+         compteur++
+        const elementLength = s.indexOf(element,compteur);
+        String(str=element.toUpperCase());
+        for (let i = 0; i < elementLength; i++) {
+        str+=element.toLowerCase();
+        }
+        
+        tabs.push(str);
+    });
+    return tabs.join('-');
 }
+//  console.log(accum());
 
-console.log(solution());
 
-// console.log(solution('camelCasing'));
-// console.log(solution('camelCasingTestC'));
+
+
+console.log(accum("ZpglnRxqenU"));
+// console.log(accum("NyffsGeyylB"));
+// console.log(accum("MjtkuBovqrU"));
+// console.log(accum("EvidjUnokmM"));
+// console.log(accum("HbideVbxncC"));
