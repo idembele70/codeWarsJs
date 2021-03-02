997537a966f93a57baa6e54704be4fb27196b114
idembele70 <idembele70@gmail.com>
2021-03-02 14:33:55 +0100

    litres

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,11 @@
-function basicOp(operation, value1, value2)
-{
-  const obj = {
-    '+' : value1 + value2,
-    '-' : value1 - value2,
-    '*' : value1 * value2,
-    '/' : value1 / value2, 
+const litres = function(time){
+    return Math.floor(time * 0.5);
   }
-  return obj[operation];
-}
 
-console.log(basicOp('+', 4, 7));
-console.log(basicOp('-', 15, 18),);
-console.log(basicOp('*', 5, 5));
-console.log(basicOp('/', 49, 7));
+  console.log(litres(2));
+  console.log(litres(1.4));
+  console.log(litres(12.3));
+  console.log(litres(0.82));
+  console.log(litres(11.8));
+  console.log(litres(1787));
+  console.log(litres(0));
