0661943fe2fdd857f05fb9da945bb57065a2c51d
idembele70 <idembele70@gmail.com>
2021-03-04 19:17:38 +0100

    squareSum js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
-const move = (position=1,roll=3) => {return position  + roll*2};
+function squareSum(numbers=[2,3,5]) {
+        return numbers.reduce((acc,i)=>{return acc+= Math.pow(i,2)},0)
 
-console.log(move(0, 4));
-console.log(move(3, 6));
-console.log(move(2, 5));
+}
+
+console.log(squareSum([1,2]), 5)
+console.log(squareSum([0, 3, 4, 5]), 50)
+console.log(squareSum([-14,-9,0,-16,-18,-18,0,1]), 1182)
