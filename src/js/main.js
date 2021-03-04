d0ee206613f476f6b47fd0ce154c91a15fe24889
idembele70 <idembele70@gmail.com>
2021-03-04 19:05:43 +0100

    move

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,5 @@
-class reversemode {
-        reverseWord(str="hello world!"){
-              return  str.split(' ').reverse().join(' ')
-        }
-}
+const move = (position=1,roll=3) => {return position  + roll*2};
 
-console.log(new reversemode().reverseWord());
+console.log(move(0, 4));
+console.log(move(3, 6));
+console.log(move(2, 5));
