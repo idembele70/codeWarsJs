d85771dd67fe3cbc060707deedde0b1342c76b1e
idembele70 <idembele70@gmail.com>
2021-03-06 16:10:25 +0100

    greet

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,7 @@
-class Abbreviation {
-        abbrevName(name='Jason Mason'){
-                const AZ = new RegExp('[A-Z]','i')
-               return  name.split` `.join(x=>x.match(AZ))
-        }
+function greet(name, owner) {
+        if (name === owner) return 'Hello boss'; 
+        else return  'Hello guest';
 }
 
-const premierAbrreviation = new Abbreviation;
-
-console.log(premierAbrreviation.abbrevName("Sam Harris"), "S.H");
-console.log(premierAbrreviation.abbrevName("Patrick Feenan"), "P.F");
-console.log(premierAbrreviation.abbrevName("Evan Cole"), "E.C");
-console.log(premierAbrreviation.abbrevName("P Favuzzi"), "P.F");
-console.log(premierAbrreviation.abbrevName("David Mendieta"), "D.M");
+console.log(greet('Daniel', 'Daniel'))
+console.log(greet('Greg', 'Daniel'))
