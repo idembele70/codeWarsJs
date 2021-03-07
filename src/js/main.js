b886184b0a011e28d092ebdb932c5b412a3ab292
idembele70 <idembele70@gmail.com>
2021-03-07 11:40:08 +0100

    cockroachspeed

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,28 @@
-const abbrevName = (name="ibrahim Dembele") =>{
-        return name.split(' ').map(x => {return x[0].toUpperCase()}).join('.');
+class RemoveClass {
+    remove(s="hii !"){
+  const re = s.lastIndexOf(s.match(/!/))
+    return s.replace(s.charAt(re), '')
 
+    }
+}
+const Myremove = (new RemoveClass);
+/* console.log((Myremove.remove()));
+
+console.log((Myremove.remove("Hi!") ), "resultat : Hi"); 
+console.log((Myremove.remove("Hi!") ), "resultat : Hi"); 
+console.log((Myremove.remove("Hi!!!") ),"resultat : Hi!!");
+console.log((Myremove.remove("!Hi") ), "resultat : !Hi");
+console.log((Myremove.remove("!Hi!") ), "resultat : !Hi");
+console.log((Myremove.remove("Hi! Hi!") ), "resultat : Hi! Hi");
+console.log((Myremove.remove("Hi")) , "resultat : Hi"); */
+
+
+// 
+
+const cockroachSpeed = function(s){
+    return ~~(s/3.6*100);
 }
 
-console.log(abbrevName("eliza doolittle"), "E.D"); // BASIC
-
-console.log(abbrevName("Sam Harris"), "S.H");
-console.log(abbrevName("Patrick Feenan"), "P.F");
-console.log(abbrevName("Evan Cole"), "E.C");
-console.log(abbrevName("P Favuzzi"), "P.F");
-console.log(abbrevName("David Mendieta"), "D.M");
+console.log(cockroachSpeed(1.08), 30);
+console.log(cockroachSpeed(1.09), 30);
+console.log(cockroachSpeed(0), 0);
