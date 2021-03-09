e5e48d0178b7ba0a15e5a6bf54399c151c2001cb
idembele70 <idembele70@gmail.com>
2021-03-09 10:41:58 +0100

    remove exclamation

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,11 @@
 class RemoveClass {
-    remove(s="hii !"){
-  const re = s.lastIndexOf(s.match(/!/))
-    return s.replace(s.charAt(re), '')
-
+    remove(s="hii ! hi!"){
+    return s.replace(/!$/, '');
     }
 }
 const Myremove = (new RemoveClass);
-/* console.log((Myremove.remove()));
-
+console.log((Myremove.remove()));
+/* 
 console.log((Myremove.remove("Hi!") ), "resultat : Hi"); 
 console.log((Myremove.remove("Hi!") ), "resultat : Hi"); 
 console.log((Myremove.remove("Hi!!!") ),"resultat : Hi!!");
@@ -17,14 +15,3 @@ console.log((Myremove.remove("Hi! Hi!") ), "resultat : Hi! Hi");
 console.log((Myremove.remove("Hi")) , "resultat : Hi"); */
 
 
-// 
-
-console.log(
-   ( (haystack= ['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false])=>{
-        return haystack.map(x =>  x === 'needle' ? "Found the needle at position " + haystack.indexOf(x) : "").join('');
-    })()
-);
-
-// les deux paire false
-// si un des deux impaire true
-// les deux pas paire true
