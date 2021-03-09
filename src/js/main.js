63a6273cd251d650161d6809ea17ad3916763148
idembele70 <idembele70@gmail.com>
2021-03-09 11:05:43 +0100

    en cours

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,14 @@
-class RemoveClass {
-    remove(s="hii ! hi!"){
-    return s.replace(/!$/, '');
-    }
+
+const countPositivesSumNegatives = (input=[]) =>{
+  let sumNegative = null, sumPositive = null;
+if (input !== null) {
+  return input;
+} else {
+  input.forEach(el => el <= 0 ? sumNegative += el : sumPositive ++);
 }
-const Myremove = (new RemoveClass);
-console.log((Myremove.remove()));
-/* 
-console.log((Myremove.remove("Hi!") ), "resultat : Hi"); 
-console.log((Myremove.remove("Hi!") ), "resultat : Hi"); 
-console.log((Myremove.remove("Hi!!!") ),"resultat : Hi!!");
-console.log((Myremove.remove("!Hi") ), "resultat : !Hi");
-console.log((Myremove.remove("!Hi!") ), "resultat : !Hi");
-console.log((Myremove.remove("Hi! Hi!") ), "resultat : Hi! Hi");
-console.log((Myremove.remove("Hi")) , "resultat : Hi"); */
+return [sumPositive, sumPositive];
+}
 
-
+console.log(countPositivesSumNegatives());
+/* console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
+console.log(countPositivesSumNegatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14])); */
