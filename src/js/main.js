1808711f4595436551b923c29ad5c8d6df217008
idembele70 <idembele70@gmail.com>
2021-03-15 12:43:44 +0100

    maps

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,6 @@
 
-const countPositivesSumNegatives = (input=[0,0]) =>{
-  let sumNegative = null, sumPositive = null;
- input.forEach(el => el <= 0 ? sumNegative+= el : sumPositive++ );
-return sumNegative !== 0 && sumPositive !== 0 ? [sumPositive, sumNegative] : [];
+function maps(x=[1, 2, 3]) {
+  return x.map(val => val * 2);
 }
 
-
-console.log(countPositivesSumNegatives());
-console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
-console.log(countPositivesSumNegatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]));
+console.log(maps());
