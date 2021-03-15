fd65b93a5ff7c77bf548aeea250b25189bf63f9e
idembele70 <idembele70@gmail.com>
2021-03-15 13:34:10 +0100

    sumArray

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
-const stringToNumber = (str="-1234")=>{
- if (!Number.isInteger(str)) {
-   return parseInt(str)
- }
+function sumArray(array=[6, 2, 1, 8, 10]) {
+  const max = Math.max(...array);
+  const min = Math.min(...array);
+  return array.reduce((acc,cur)=>acc+=cur)-max-Math.min(...array)
 }
-console.log(stringToNumber());
+
+console.log(sumArray());
