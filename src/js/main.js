ae7735b93fd75379f7a153a621281afcc18607bb
idembele70 <idembele70@gmail.com>
2021-03-19 12:55:19 +0100

    countpositivesSumNegatives

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
-function countPositivesSumNegatives(input) {
-  function countPositivesSumNegatives(input) {
+function countPositivesSumNegatives(input=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]) {
     let sumNegative = 0, sumPositive = 0;
       if(input !== null)input.forEach(el => el <= 0 ? sumNegative+= el : sumPositive++ );
     return sumNegative !== 0 && sumPositive !== 0 ? [sumPositive, sumNegative] : [];
     }
 
+console.log(countPositivesSumNegatives());
