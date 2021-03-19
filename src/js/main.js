30717b08f6d901f368ca69bd2ca521b6c269e639
idembele70 <idembele70@gmail.com>
2021-03-19 12:35:24 +0100

    sumnation

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,9 @@
-function sumArray(array=[6, 2, 1, 8, 10]) {
-  const max = Math.max(...array);
-  const min = Math.min(...array);
- let acc = Number();
- if (array == null || array.length <= 1)return 0 ;
- else for (let i = 0; i < array.length; i++)acc += array[i]; return acc-min-max;
+const sumnation = function(num=3){
+  let acc = null;
+for (let i = 1; i < num; i++) {
+  acc+=i
+}
+return acc
 }
 
-console.log(sumArray());
-
-// return array === null || array.length === 0 || array.length === 1 ? 0 : array.reduce((acc,cur)=>acc+=cur,0)-(Math.max(...array))-Math.min(...array);
+console.log(sumnation());
