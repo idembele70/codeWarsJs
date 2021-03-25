19532ea9fac6467461118bab18f2ce84e1a3cbce
idembele70 <idembele70@gmail.com>
2021-03-25 13:56:44 +0100

    fakebin avec for if

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,13 @@
 function fakeBin(x='45385593107843568') {
- return   x.split('').map(x => x < 5 ? 0 : 1).join``;
+    xToBin = ''
+ for (let i = 0; i < x.length; i++) {
+     if (x[i] < 5) {
+       xToBin += 0;
+    } else {
+        xToBin += 1;
+     }
+    }
+    return xToBin;
 }
 
 console.log(fakeBin());
