e0657c9b8548cfb90c5c5e7a720d40dcd6efb40c
idembele70 <idembele70@gmail.com>
2021-03-25 13:47:07 +0100

    avec split et map

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,5 @@
 function fakeBin(x='45385593107843568') {
-    let strToBin = '';
-    x.split('').forEach(element => {
-       strToBin += element < 5 ? 0 : 1;
-   });
-   return strToBin
+ return   x.split('').map(x => x < 5 ? 0 : 1).join``;
 }
 
 console.log(fakeBin());
