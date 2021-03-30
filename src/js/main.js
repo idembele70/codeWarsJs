64b49bb94dc9242b22c35799da566af99a190eeb
idembele70 <idembele70@gmail.com>
2021-03-30 18:36:56 +0200

    beginner series #2 Clock  avec des variable clair

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,11 @@ __ beginner series #2 Clock
 
 
 const past = (h = 0, m = 1, s = 1) => {
-    return (h * 3600 + m * 60 + s) * 1000;
+    const hourToSecondes = h * 3660,
+        minuteToSecondes = m * 60,
+        seconde = s,
+        secondeToMilliseconde = 1000;
+    return (hourToSecondes + minuteToSecondes + seconde) * secondeToMilliseconde;
 }
 
 console.log(past());
