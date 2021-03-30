90d922c613820d8b9c08410b718d55f23f0cabbd
idembele70 <idembele70@gmail.com>
2021-03-30 19:22:40 +0200

    avec reduce

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,5 @@
-class Point {
-    static points(games = ['1:0', '2:0', '3:0', '4:0', '2:1', '3:1', '4:1', '3:2', '4:2', '4:3']) {
-        return games.map(element => {
-            const scoreTabs = element.split(/\D/);
-            let x = scoreTabs[0], y = scoreTabs[1];
-            if (x > y) {
-                return element = 3;
-            } else if (x == y) {
-                return element = 1;
-            } else {
-               return element = 0;
-            }
-
-        }).reduce((acc, val) => acc += val);
-    }
+const getAverage = function(marks=[2,2,2,2]){
+    return Math.floor(marks.reduce((acc,val) => acc+=val) / marks.length);
 }
-
-console.log(Point.points());
+console.log(getAverage());
+console.log(getAverage([1,2,3,4,5,]));
