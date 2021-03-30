5cce5821c930166b09c0972a3a17531bbfc17861
idembele70 <idembele70@gmail.com>
2021-03-30 19:11:35 +0200

    avec map et reduce

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,17 @@
 class Point {
-  static points(games=['1:0','2:0','3:0','4:0','2:1','3:1','4:1','3:2','4:2','4:3']){
-      let point = 0;
-         games.forEach(matchPoint => {
-            return matchPoint.split(/:/)[0] > matchPoint.split(/:/)[1]
-            ? point +=3 : matchPoint.split(/:/)[0] == matchPoint.split(/:/)[1]
-            ? point += 1 : point += 0;
-        })
-        return point;
+    static points(games = ['1:0', '2:0', '3:0', '4:0', '2:1', '3:1', '4:1', '3:2', '4:2', '4:3']) {
+        return games.map(element => {
+            const scoreTabs = element.split(/\D/);
+            let x = scoreTabs[0], y = scoreTabs[1];
+            if (x > y) {
+                return element = 3;
+            } else if (x == y) {
+                return element = 1;
+            } else {
+               return element = 0;
+            }
+
+        }).reduce((acc, val) => acc += val);
     }
 }
 
