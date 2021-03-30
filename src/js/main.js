d840c13dbaa82350fe4eb4ef74f3430fa0e2e1e7
idembele70 <idembele70@gmail.com>
2021-03-30 19:25:03 +0200

    avec for of

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
-const getAverage = function(marks=[2,2,2,2]){
-    return Math.floor(marks.reduce((acc,val) => acc+=val) / marks.length);
+const getAverage = function (marks = [2, 2, 2, 2]) {
+    let result = null;
+    for (const note of marks) {
+       result += note;
+    }
+    return  Math.floor(result / marks.length);
 }
 console.log(getAverage());
-console.log(getAverage([1,2,3,4,5,]));
+console.log(getAverage([1, 2, 3, 4, 5,]));
