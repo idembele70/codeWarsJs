1f8358ea1561fced365239fe33447713f305813e
idembele70 <idembele70@gmail.com>
2021-04-14 17:56:00 +0200

    propre

--- src/js/main.js
+++ src/js/main.js
@@ -2,22 +2,16 @@ function rps(p1 = "rock", p2 = 'scissors') {
   switch (p1 + p2) {
     case "scissorspaper":
       return getMsg(1);
-      break;
     case "paperscissors":
       return getMsg(2);
-      break;
     case "paperrock":
       return getMsg(1);
-      break;
     case "rockpaper":
       return getMsg(2);
-      break;
     case "rockscissors":
       return getMsg(1);
-      break;
     case "scissorsrock":
       return getMsg(2);
-      break;
 
     default: return "Draw!"
   }
