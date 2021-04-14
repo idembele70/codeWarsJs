6972c1ce35c79140da5d41431c9ac4c7bb2313c6
idembele70 <idembele70@gmail.com>
2021-04-14 15:40:37 +0200

    avec getMsg

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,27 @@
 function rps(p1 = "rock", p2 = 'scissors') {
 
   if (p1 + p2 === 'paperscissors') {
-    return 'Player 2 won!'
+    return getMsg(2)
   } else if (p1 + p2 === 'scissorspaper') {
-    return 'Player 1 won!'
+    return getMsg(1)
   } else if (p1 + p2 === 'rockpaper') {
-    return 'Player 2 won!'
+    return getMsg(2)
   }
   else if (p1 + p2 === 'paperrock') {
-    return 'Player 1 won!'
+    return getMsg(1)
   }
   else if (p1 + p2 === 'scissorsrock') {
-    return 'Player 2 won!'
+    return getMsg(2)
   }
   else if (p1 + p2 === 'rockscissors') {
-    return 'Player 1 won!'
+    return getMsg(1)
   } else {
     return 'Draw!'
   }
+}
+
+const getMsg = (n) => {
+  return (n == 1 || n == 2) ? `Player ${n} won!` : 'Draw'
 }
 
 console.log(rps());
