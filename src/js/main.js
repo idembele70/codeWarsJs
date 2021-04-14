3a9e5bd10bd5d6b21c1fc6f1ec5ddba6c0f8ec5f
idembele70 <idembele70@gmail.com>
2021-04-14 14:26:08 +0200

    avec foreach et forof

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,11 @@
 class Array {
-  static arrayPlusArray(arr = [1, 2, 3], arr2 = [4, 5, 6]) {
-    return arr.reduce((acc, v) => acc += v) + arr2.reduce((acc, v) => acc += v);
+  static arrayPlusArray(arr1 = [1, 2, 3], arr2 = [4, 5, 6]) {
+    let sum1 = 0, sum2 = 0;
+    arr1.forEach(el => sum1 += el);
+    for (const item of arr2) {
+      sum2 += item;
+    }
+    return sum1 + sum2
   }
 }
 
