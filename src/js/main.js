0bce470e76a2600e360a68d79784178e28debf8f
idembele70 <idembele70@gmail.com>
2021-04-14 14:31:55 +0200

    grow avec reduce

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,5 @@
-class Array {
-  static arrayPlusArray(arr1 = [1, 2, 3], arr2 = [4, 5, 6]) {
-    let sum1 = 0, sum2 = 0;
-    arr1.forEach(el => sum1 += el);
-    for (const item of arr2) {
-      sum2 += item;
-    }
-    return sum1 + sum2
-  }
+const grow = function(x=[1, 2, 3]){
+  return x.reduce((prev,curr)=>prev*=curr)
 }
 
-console.log(Array.arrayPlusArray());
+console.log(grow());
