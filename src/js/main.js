56f08a5e6adf6834c6dd7daedcd8d60861a2c856
idembele70 <idembele70@gmail.com>
2021-04-14 14:54:51 +0200

    juste manière de ce compliquer la vie

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,13 @@
-const DNAToRNA = (dna="GCAT")=>{
-  return dna.replace(/T/g,'U');
+const DNAToRNA = (dna = "GCAT") => {
+  let dnaLength = 0;
+  if (dna.match(/T/)) {
+    dnaLength = dna.match(/T/g).length
+  }
+  for (let index = 0; index < dnaLength; index++) {
+    const URegex = new RegExp("T");
+    dna = dna.replace(URegex, 'U');
+  }
+  return dna;
 }
 
 console.log(DNAToRNA());
