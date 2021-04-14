fce6174d526a0edf280d68be18457fdd96193df6
idembele70 <idembele70@gmail.com>
2021-04-14 14:37:46 +0200

    grow avec for

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
-const grow = function(x=[1, 2, 3]){
-  return x.reduce((prev,curr)=>prev*=curr)
+const grow = function (x = [1, 2, 3]) {
+  let res =0;
+  for (let a = 0; a < x.length; a++) {
+    res *= x[a]
+    if (a + 1 == x.length) return res;
+  }
 }
 
 console.log(grow());
