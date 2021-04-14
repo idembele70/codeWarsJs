c5b8745f6926b4b4704e3128e5ee89ad601855bc
idembele70 <idembele70@gmail.com>
2021-04-14 15:13:04 +0200

    rock paper scissors avec else if

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,23 @@
-const DNAToRNA = (dna = "GCAT") => {
-  let dnaLength = 0;
-  if (dna.match(/T/)) {
-    dnaLength = dna.match(/T/g).length
+function rps(p1 = "rock", p2 = 'scissors') {
+
+  if (p1 + p2 === 'paperscissors') {
+    return 'Player 2 won!'
+  } else if (p1 + p2 === 'scissorspaper') {
+    return 'Player 1 won!'
+  } else if (p1 + p2 === 'rockpaper') {
+    return 'Player 2 won!'
   }
-  for (let index = 0; index < dnaLength; index++) {
-    const URegex = new RegExp("T");
-    dna = dna.replace(URegex, 'U');
+  else if (p1 + p2 === 'paperrock') {
+    return 'Player 1 won!'
   }
-  return dna;
+  else if (p1 + p2 === 'scissorsrock') {
+    return 'Player 2 won!'
+  }
+  else if (p1 + p2 === 'rockscissors') {
+    return 'Player 1 won!'
+  } else {
+    return 'Draw!'
+  }
 }
 
-console.log(DNAToRNA());
+console.log(rps());
