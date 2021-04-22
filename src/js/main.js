452ef7abb56669ae60837a9eca947c530b30a131
idembele70 <idembele70@gmail.com>
2021-04-22 18:45:27 +0200

    avec un for

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
-const betterThanAverage = function(classPoints=[2, 3], yourPoints=5){
-  return classPoints.reduce((acc,curr)=>acc+=curr) / classPoints.length < yourPoints
+const betterThanAverage = function (classPoints = [2, 3], yourPoints = 5) {
+  let sum = 0, average = 0;
+  const numberOfStudents = classPoints.length;
+  for (let i = 0; i < classPoints.length; i++) {
+    sum += classPoints[i];
+  }
+  average = sum / numberOfStudents;
+  return average < yourPoints;
 }
 
 console.log(betterThanAverage());
