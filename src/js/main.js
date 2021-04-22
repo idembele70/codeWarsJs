ad5cd7d432cbcc196b2372130498fbb746ff5f58
idembele70 <idembele70@gmail.com>
2021-04-22 19:23:39 +0200

    avec filter

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
-const betterThanAverage = function (classPoints = [2, 3], yourPoints = 5) {
-  let sum = 0, average = 0;
-  const numberOfStudents = classPoints.length;
-  
-    classPoints.forEach(numero => sum += numero);
-  average = sum / numberOfStudents;
-  return average < yourPoints;
+class FindIt {
+  static firstNonConsecutive(arr = [ 1,2,3 ]) {
+    let NumberNonConsecutive = arr.filter((val, pos) => {
+      return arr[pos - 1] != null && val - 1 != arr[pos - 1]
+    })
+    return NumberNonConsecutive.length == 0 ? null : parseInt(NumberNonConsecutive)
+  }
 }
 
-console.log(betterThanAverage());
+console.log(FindIt.firstNonConsecutive());
