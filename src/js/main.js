b39081bf84288c54915f7c3904235ad4876c4e2f
idembele70 <idembele70@gmail.com>
2021-04-22 18:46:35 +0200

    betterThanAverage avec forEach

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,8 @@
 const betterThanAverage = function (classPoints = [2, 3], yourPoints = 5) {
   let sum = 0, average = 0;
   const numberOfStudents = classPoints.length;
-  for (let i = 0; i < classPoints.length; i++) {
-    sum += classPoints[i];
-  }
+  
+    classPoints.forEach(numero => sum += numero);
   average = sum / numberOfStudents;
   return average < yourPoints;
 }
