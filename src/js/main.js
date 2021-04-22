19f29dd1dfc5aff4fd7d3afd15226bbb6e636a80
idembele70 <idembele70@gmail.com>
2021-04-22 18:38:41 +0200

    function greet name

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,5 @@
-function rps(p1 = "rock", p2 = 'scissors') {
-  switch (p1 + p2) {
-    case "scissorspaper":
-      return getMsg(1);
-    case "paperscissors":
-      return getMsg(2);
-    case "paperrock":
-      return getMsg(1);
-    case "rockpaper":
-      return getMsg(2);
-    case "rockscissors":
-      return getMsg(1);
-    case "scissorsrock":
-      return getMsg(2);
-
-    default: return "Draw!"
-  }
+function greet(name = "Ryan") {
+  return ("Hello, " + name + " how are you doing today?");
 }
-const getMsg = (n) => `Player ${n} won!`;
-console.log(rps(p1 = "paper", p2 = "scissors"));
+
+console.log(greet());
