8a0ad6a73b5f51c6272608c832d961984cd4370a
idembele70 <idembele70@gmail.com>
2021-04-22 20:22:00 +0200

    firstnonconsecutive avec for

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,20 @@
 class FindIt {
-  static firstNonConsecutive(arr = [ 1,2,3 ]) {
-    let NumberNonConsecutive = arr.filter((val, pos) => {
-      return arr[pos - 1] != null && val - 1 != arr[pos - 1]
-    })
-    return NumberNonConsecutive.length == 0 ? null : parseInt(NumberNonConsecutive)
+  static firstNonConsecutive(arr = [9, 10, 11, 12, 13, 14, 15, 16, 17]) {
+    let pre = 'str', finalVal = true;
+    for (let i = 0; i < arr.length; i++) {
+      if (pre == 'str') {
+        pre = arr[i];
+        continue
+      } else if (pre != arr[i]-1) {
+        pre = arr[i];
+        finalVal = false;
+        break;
+      } else {
+        pre = arr[i]
+        continue
+      }
+    }
+    return finalVal ? null : pre
   }
 }
 
