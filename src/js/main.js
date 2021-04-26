4c9c897dcc5d9729e40c15ee758de99be2979d2d
idembele70 <idembele70@gmail.com>
2021-04-27 01:51:41 +0200

    islocknessMonster

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,8 @@
-function ValidateUsr(username="ass") {
-  const res = new RegExp('[a-z]|_|\\d','g');
-  if (username) {
-   return username.match(res).length == username.length && 4<=username.length<=16;
-  } else {
-    return false
-  }
+function isLockNessMonster(s) {
+  const reg = new RegExp("tree fiddy|3.50");
+  return reg.test(s)
 }
 
-console.log(ValidateUsr());
+console.log(isLockNessMonster("Your girlscout cookies are ready to ship. Your total comes to tree fiddy"));
+console.log(isLockNessMonster("Your girlscout cookies are ready to ship. Your total comes to 3.50"));
+console.log(isLockNessMonster("Yo, I heard you were on the lookout for Nessie. Let me know if you need assistance."));
