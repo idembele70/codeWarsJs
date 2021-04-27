de6bf42e9563463b874e0675d0ff2868b33c4254
idembele70 <idembele70@gmail.com>
2021-04-27 13:17:53 +0200

    distance between two points

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,23 @@
-const array = (arr = '1,2,3') => {
- const newarr = arr.toString();
-  return newarr.match(/,{2,}/g) && newarr.match(/,/g).length >=2 ? newarr.split(/,/).slice(1,-1).join` ` : null ;
+/* function logicalCalc(array = [true, true, true, false], op = "AND") {
+
+  switch (op) {
+    case "AND":
+      array.join` `.match(/false/);
+      return true
+    case "XOR":
+    case "OR":
+      array.match(/true/);
+      return true
+
+    default:
+      return "Your operator doesn't exist !"
+  }
 }
 
-console.log(array());
+console.log(logicalCalc()); */
+
+function distanceBetweenPoints(a = { x: 3, y: 3 }, b = { x: 3, y: 3 }) {
+  return Math.hypot(a.x - b.x, a.y - b.y)
+}
+
+console.log(distanceBetweenPoints());
