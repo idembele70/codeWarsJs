9537cd23da03c3621462b0f0463c9c250c00e2c6
idembele70 <idembele70@gmail.com>
2021-04-28 13:27:11 +0200

    logical calc

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,11 @@
 function logicalCalc(array = [true, true], op = "AND") {
-  let res = Boolean();
-  for (let i = 0; i < array.length; i++) {
-    if (op == "AND") {
-      return eval(array.join` && `);
-    } else if (op == "OR") {
-      return eval(array.join` |`);
-    } else if (op = "XOR") {
-      return eval(array.join`^`);
-    }
+  if (op == "AND") {
+    return eval(array.join` && `);
+  } else if (op == "OR") {
+    return eval(array.join` |`);
+  } else if (op = "XOR") {
+    return eval(array.join`^`);
   }
-  return false;
 }
 
 console.log(logicalCalc());
