1ef58d40bdc0bd2c9145e020a43f65eb7b420be6
idembele70 <idembele70@gmail.com>
2021-04-30 14:12:12 +0200

    take avec for

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,15 @@
-function take(arr=[],n=1) {
-  return arr.slice(0,n)
+function take(arr = [], n = 1) {
+  let tabs = []
+  console.log(arr)
+if(arr == null)return tabs;
+else
+{
+for (let i = 0; i < n; i++) {
+  if(arr[i] != undefined)
+  tabs.push(arr[i]);
+}
+return tabs
+}
 }
 
 console.log(take([0, 1, 2, 3, 5, 8, 13], 3));
