da5de64448d1d1933fb1ace1a1ca00a4c2650581
idembele70 <idembele70@gmail.com>
2021-05-03 01:34:39 +0200

    vowelindice

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
-function sortBytes(uint32 = 3735928559) {
-  const arrUint32 = uint32.toString(2).padStart(32, 0).split('').map((x, i) => (i + 1) % 8 == 0 && i != 0 ? x += " " : x).join``.trim().split(' ').sort((a,b)=>b-a).join``;
-  return  parseInt(arrUint32,2)
+function vowelIndices(word = "orange") {
+  const res = []
+   word.split('').map((char, index) => !!char.match(/a|e|y|u|i|o/i) ? res.push(index+1) : null)
+   return res
 }
-console.log(sortBytes());
+console.log(vowelIndices());
