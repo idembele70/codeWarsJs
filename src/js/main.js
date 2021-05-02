96723ca64db1717080abd4fceb5afaf3e595a333
idembele70 <idembele70@gmail.com>
2021-05-03 01:45:26 +0200

    vowel indices avec for

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,13 @@
 function vowelIndices(word = "orange") {
-  return word.split('').map((char, index) => !!char.match(/[aeyuio]/i) ? index + 1 : "").filter(x=> x !="")
+  const vowIndices = [];
+  for (let i = 0; i < word.length; i++) {
+    const char = word.charAt(i);
+    if (char.match(/[aeyuio]/i)) {
+      vowIndices.push(++i)
+      i--
+    }
+  }
 
+  return vowIndices;
 }
 console.log(vowelIndices());
