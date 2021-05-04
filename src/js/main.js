f1201b004718f221d2cb7561f89d0208af2aa954
idembele70 <idembele70@gmail.com>
2021-05-04 11:28:17 +0200

    validSpacing avec regex

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,5 @@
-function reverseBit(n = 417) {
-  const nToBinaire = n.toString(2);
-  let nBinaireReversed = "";
-  for (let i = nToBinaire.length; i > 0;) {
-    i--
-    nBinaireReversed += nToBinaire[i];
-  }
-  console.log(nBinaireReversed);
-  const nReversed = parseInt(nBinaireReversed, 2);
-  return nReversed;
+const validSpacing = (s = "K  World") => {
+  return !(/^ /.test(s) || / $/.test(s) || / {2,}/.test(s))
 }
-console.log(reverseBit());
+
+console.log(validSpacing());
