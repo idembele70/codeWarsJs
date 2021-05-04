b0290a397a4b92a7bf012e8e2b3548ec5c94a9e8
idembele70 <idembele70@gmail.com>
2021-05-04 11:17:30 +0200

    avec push

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 function reverseBit(n = 417) {
   const nToBinaire = n.toString(2);
   let nBinaireReversed = [];
-  for (let i = 0; i < nToBinaire.length; i++) {
-    nBinaireReversed.unshift(nToBinaire[i])
+  for (let i = nToBinaire.length; i >= 0; i--) {
+    nBinaireReversed.push(nToBinaire[i])
   }
   const nReversed = parseInt(nBinaireReversed.join``, 2);
   return nReversed
