e89c75322d832674311b96666feea16743350cb3
idembele70 <idembele70@gmail.com>
2021-05-04 11:19:18 +0200

    for V2

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 function reverseBit(n = 417) {
   const nToBinaire = n.toString(2);
-  let nBinaireReversed = [];
-  for (let i = nToBinaire.length; i >= 0; i--) {
-    nBinaireReversed.push(nToBinaire[i])
+  let nBinaireReversed = "";
+  for (let i = nToBinaire.length; i > 0;) {
+    i--
+    nBinaireReversed += nToBinaire[i];
   }
-  const nReversed = parseInt(nBinaireReversed.join``, 2);
-  return nReversed
+  console.log(nBinaireReversed);
+  const nReversed = parseInt(nBinaireReversed, 2);
+  return nReversed;
 }
 console.log(reverseBit());
