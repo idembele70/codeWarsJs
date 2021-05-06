aed15ce9ed8743a2ce9b353582fc3474b65d0163
idembele70 <idembele70@gmail.com>
2021-05-06 10:46:47 +0200

    alphabet simetry

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,11 @@
-function cleanString(s = "abc#d##c") {
-  const cleanedString = [];
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] == "#") {
-      if (cleanedString == "") {
-
-      } else {
-        cleanedString.pop()
-      }
-    } else {
-      cleanedString.push(s[i])
-    }
-  }
-  return cleanedString.join``;
+function solve(arr = ["abode", "ABc", "xyzD"]) {
+  const tabs =
+    arr.map(x => {
+      return x.toString().split('').filter((a, i) => {
+        return (a.toLowerCase().charCodeAt(0) - 97) == i
+      }).length
+    })
+  return tabs;
 }
-console.log(cleanString());
+
+console.log(solve());
