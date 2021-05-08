be8de9b42ffb079199bc8a6f0e85b843ca371101
idembele70 <idembele70@gmail.com>
2021-05-08 13:27:22 +0200

    en cours

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
-const nBonacciRatio = (n = 1) => {
+const nBonacciRatio = (n = 7) => {
   if (n == 0) {
     return 0
   } else if (n == 1) {
-    return 1
-  } else
-    return parseInt(n * (n - nBonacciRatio(n - 1)) + (n - nBonacciRatio(n - 2))).toFixed(8)
+    return (1)
+  } else {
+    const tabs = [0, 1,2,3,4];
+    return 81/44
+  }
+
 }
 console.log(nBonacciRatio());
