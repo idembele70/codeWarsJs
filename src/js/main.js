c62db585b5aa04f42f93cd23ab90531e38d0cf99
idembele70 <idembele70@gmail.com>
2021-05-13 02:08:41 +0200

    on

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,33 @@
-function multiTable(number = 5) {
-  let res = '';
-  for (let i = 1; i <= 10; ++i) {
-    if (res == '') res += i + ' * ' + number + ' = ' + number * i;
-    else res += '\n' + i + ' * ' + number + ' = ' + number * i;
+const getPositions = (s = 55) => {
+  const handsUpTabs = { P1: 0, P2: 0, P3: 0 };
+  let step = 0;
+  for (let i = 0; i < s; i++) {
+    const res = [];
+    switch (true) {
+      case i % 3 == 0:
+        handsUpTabs.P1 = 0;
+        break;
+      case i % 9 == 0:
+        handsUpTabs.P1 = 0;
+        handsUpTabs.P2 = 0;
+        handsUpTabs.P3++;
+        break;
+      case i % 3 !== 0:
+        handsUpTabs.P1++;
+        break;
+      case i % 3 !== 0:
+        handsUpTabs.P1++;
+        break;
+
+      default:
+        break;
+    }
+    for (const part in handsUpTabs) {
+      res.push(handsUpTabs[part]);
+    }
+    console.log("step " + step + " : " + res);
+    ++step;
   }
-  return res
-}
-console.log(multiTable());
+};
+
+getPositions();
