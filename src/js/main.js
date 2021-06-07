d1348aa60504d56af6cd7d3e61b7f39335674b51
idembele70 <idembele70@gmail.com>
2021-06-08 00:42:35 +0200

    mode hard

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
-function friend(friends=["Ryan", "Kieran", "Mark"]) {
-  const myFriends = []
-   for (const name of friends) {
-     if(name.length === 4) myFriends.push(name)
-   }
-   return myFriends
+function divisors(integer = 13) {
+  const res =  [...Array(integer)].map((x,i)=>x=i).filter((x, i) => {
+    if (integer % i == 0 && x!= 1) return (x = i);
+  });
+  return res.length == 0? integer+" is prime" : res
 }
-console.log(friend());
+
+console.log(divisors());
