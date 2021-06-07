0c40beb3f9b769574b103d7f7d85280f1b38d1f8
idembele70 <idembele70@gmail.com>
2021-06-08 00:46:40 +0200

    AVEC FOR

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
 function divisors(integer = 13) {
-  const res =  [...Array(integer)].map((x,i)=>x=i).filter((x, i) => {
-    if (integer % i == 0 && x!= 1) return (x = i);
-  });
-  return res.length == 0? integer+" is prime" : res
+  const tabs = [];
+  for (let i = 2; i < integer; i++) {
+    if (integer % i == 0) tabs.push(i);
+  }
+  return tabs.length == 0 ? `${integer} is prime` : tabs;
 }
 
 console.log(divisors());
