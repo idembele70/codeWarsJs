370008915d1f77b04487cc79f7dd33e9c84783bf
idembele70 <idembele70@gmail.com>
2021-06-07 07:47:57 +0200

    friend kata

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,8 @@
-function openOrSenior(
-  data = [
-    [45, 12],
-    [55, 21],
-    [19, -2],
-    [104, 20],
-  ]
-) {
-    const tabs = []
-  for (let i = 0; i < data.length; i++) {
-      tabs.push(
-          data[i][0] >= 55 && data[i][1] > 7 ? "Senior" : "Open"
-      )
-      
-  }
-  return tabs
+function friend(friends=["Ryan", "Kieran", "Mark"]) {
+  const myFriends = []
+   for (const name of friends) {
+     if(name.length === 4) myFriends.push(name)
+   }
+   return myFriends
 }
-console.log(openOrSenior());
+console.log(friend());
