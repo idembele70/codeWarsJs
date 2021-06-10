1cdd3ccc38b5d474c94fd5183302c6322aea6b75
idembele70 <idembele70@gmail.com>
2021-06-11 01:14:07 +0200

    sortArray

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,16 @@
-function booleanToString(b=true) {
-  return ""+b
+function sortArray(array = [1, 7]) {
+  const oddTabs = [],
+    newArray = [];
+  let i = 0;
+  array.forEach((number) => (number % 2 != 0 ? oddTabs.push(number) : null));
+  oddTabs.sort((a,b)=>a-b);
+  array.forEach((num) => {
+    if (num % 2 != 0) {
+      newArray.push(oddTabs[i]);
+      i++;
+    } else newArray.push(num);
+  });
+  return newArray;
 }
-console.log(booleanToString());
+
+console.log(sortArray([5, 8, 6, 3, 4]));
