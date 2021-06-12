ae8d16292a65bd81f0a351d3fab6586592424cf5
idembele70 <idembele70@gmail.com>
2021-06-13 00:22:25 +0200

    check alive

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,5 @@
-function humanYearsCatYearsDogYears(humanYearsCatYearsDogYears = 1) {
-  const humanYears = humanYearsCatYearsDogYears;
-  let catYears = 0;
-  let dogYears = 0;
-
-  for (let i = 1; i <= humanYears; i++) {
-    if (i == 1) {
-      catYears += 15;
-      dogYears += 15;
-    }
-   else if (i == 2) {
-      catYears += 9;
-      dogYears += 9;
-    } else {
-      catYears += 4;
-      dogYears += 5;
-    }
-  }
-
-  return [humanYears, catYears, dogYears];
+function checkAlive(health) {
+  return !(health <= 0 || health >=10)
 }
 
-console.log(humanYearsCatYearsDogYears());
+console.log(checkAlive(5));
