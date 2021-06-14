c70a0e0281a509677f41c00b4ad0a04f88328961
idembele70 <idembele70@gmail.com>
2021-06-14 22:05:50 +0200

    find squares

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
-function checkAlive(health) {
-  return !(health <= 0 || health >=10)
+function findSquares(num = 9) {
+  const midInteger = num / 2;
+  return Math.ceil(midInteger) ** 2 + "-" + Math.floor(midInteger) ** 2;
 }
 
-console.log(checkAlive(5));
+console.log(findSquares(5));
