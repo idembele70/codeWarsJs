39491a864ae9965c145aa0e01c4ed6933c8936fd
idembele70 <idembele70@gmail.com>
2021-06-15 23:54:04 +0200

    mode non strick

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
-function findSquares(num = 9) {
-  const midInteger = num / 2;
+function findSquares(num) {
+  const midInteger = (num / 2)**2;
   return Math.ceil(midInteger) ** 2 + "-" + Math.floor(midInteger) ** 2;
 }
-
 console.log(findSquares(5));
