8629e9cacc0ca6c9ba19db275b3d1cb0e8af74c1
idembele70 <idembele70@gmail.com>
2021-06-17 00:50:14 +0200

    fini

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 console.log(
-  (function quarterOf(month = 3) {
-    return month  %2 == 0 ? Math.floor(month  / 3) : Math.ceil(month/3)
+  (function quarterOf(month = 4) {
+    return Math.ceil(month / 3)
   })()
 );
