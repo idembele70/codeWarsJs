05c438b193d2fff4fe2e7de1477da6a2c16370ba
idembele70 <idembele70@gmail.com>
2021-06-21 01:19:10 +0200

    reversed word sur une ligne

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,5 @@
-function makeUpperCasse(str = "hello worLD") {
-  return [...str].map((x, i) => {
-    const upperCassed = x.charCodeAt(0) - 32;
-    if (x == " ") return " ";
-    return x.charCodeAt(0) > 90 ? (x = String.fromCharCode(upperCassed)) : x;
-  }).join``;
-}
-
-console.log(makeUpperCasse());
-console.log("z".charCodeAt(0));
+console.log(
+  (function reverseWords(str = "apple spaced") {
+    return str.split(" ").map((x) => [...x].reverse().join("")).join` `;
+  })()
+);
