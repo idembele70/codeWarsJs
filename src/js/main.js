4c3730df59af1db11e1bc84e809a85e347462ce7
idembele70 <idembele70@gmail.com>
2021-06-21 01:26:47 +0200

    avec une boucle for

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
 console.log(
   (function reverseWords(str = "apple spaced") {
-    return str.split(" ").map((x) => [...x].reverse().join("")).join` `;
+    const strResversed = [];
+    let word = "";
+    for (let i = 0; i < str.length; i++) {
+      const charAtX = str[i];
+      if (charAtX == " " || i + 1 == str.length) {
+        if (i + 1 == str.length) word += charAtX;
+        strResversed.push(word.split``.reverse().join``);
+        word = "";
+      } else {
+        word += charAtX;
+      }
+    }
+    return strResversed.join` `;
   })()
 );
