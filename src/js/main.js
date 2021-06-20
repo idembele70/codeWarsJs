c45cee97469c1c1029b1d7824b0c4f81a0a308bb
idembele70 <idembele70@gmail.com>
2021-06-20 14:38:20 +0200

    un peu complexe

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,10 @@
-function makeUpperCasse(str = "hello") {
-  return str.toLocaleUpperCase();
+function makeUpperCasse(str = "hello worLD") {
+  return [...str].map((x, i) => {
+    const upperCassed = x.charCodeAt(0) - 32;
+    if (x == " ") return " ";
+    return x.charCodeAt(0) > 90 ? (x = String.fromCharCode(upperCassed)) : x;
+  }).join``;
 }
 
 console.log(makeUpperCasse());
+console.log("z".charCodeAt(0));
