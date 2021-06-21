e2c6e56eb81095e5d3f099e50d542b3abfd05e39
idembele70 <idembele70@gmail.com>
2021-06-22 00:36:38 +0200

    une ligne

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,5 @@
-const number = (busStops=[[10,0],[3,5],[5,8]]) => {
-  let PeopleAtTheEnd = 0;
-  for (const stop of busStops) {
-    PeopleAtTheEnd+=stop[0]-stop[1]
-  } 
- return PeopleAtTheEnd
+function countSheep(num = 3) {
+  return [...Array(num).keys()].map(x=>`${x+1} sheep...`).join``
 }
 
-console.log(number());
+console.log(countSheep());
