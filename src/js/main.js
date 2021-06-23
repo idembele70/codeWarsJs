a7312085b783c05964d9d9f94df3e2c16e2e1d07
idembele70 <idembele70@gmail.com>
2021-06-24 00:43:26 +0200

    en cours

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
-function closeCompare(a=3,b=5, margin=3) {
-  margin = margin == undefined  || margin < 0  ? 0 : margin ;
-  return a - b < margin && margin !=0 ? 0 : a > b ?  -1 : 1
+function closeCompare(a = 2, b = 5, margin=3) {
+  console.log(a, b, margin);
+  margin = margin == undefined || margin < 0 ? 0 : margin;
+  console.log(margin);
+  if (a - b <= margin && margin != 0 || a-b==0 ) return 0;
+  else if (a < b) return -1;
+  return 1;
 }
 console.log(closeCompare());
