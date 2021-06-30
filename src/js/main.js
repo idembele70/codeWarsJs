cce4fe4d19f51762361c3043a9a74677bba03ad9
idembele70 <idembele70@gmail.com>
2021-07-01 00:49:57 +0200

    sp english

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,5 @@
-function getPositions(s = 3) {
-  const Step = [0, 0, 0];
-  return [~~(s%3), ~~(s/3) %3, ~~(s/9)%3]
+function spEng(sentence="english") {
+ return /english/ig.test(sentence) 
 }
-console.log(getPositions());
-console.log(getPositions(54));
-console.log(getPositions(98));
 
+console.log(spEng());
