9e8b1e9c5e89998a7974d57ae5f53c7bb6fd26c5
idembele70 <idembele70@gmail.com>
2021-07-02 00:50:26 +0200

    with for

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,15 @@
-function spEng(sentence="english") {
-  const word = new RegExp('english', 'i')
- return !!sentence.match(word)
+function stringToArray(string = "Robin Singh") {
+  const result = [];
+  let word = "";
+  for (let i = 0; i < string.length; i++) {
+    const currentChar = string[i];
+    if (currentChar == " ") {
+      result.push(word);
+      word = "";
+    } else word += currentChar;
+  }
+  result.push(word);
+  return result;
 }
 
-console.log(spEng());
+console.log(stringToArray());
