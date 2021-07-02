e7785b9b9e8600c1ffd156a1c2bba5ebdc301363
idembele70 <idembele70@gmail.com>
2021-07-03 00:45:45 +0200

    avec filter

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,7 @@
-function stringToArray(string = "Robin Singh") {
-  const result = [];
-  let word = "";
-  for (let i = 0; i < string.length; i++) {
-    const currentChar = string[i];
-    if (currentChar == " ") {
-      result.push(word);
-      word = "";
-    } else word += currentChar;
-  }
-  result.push(word);
-  return result;
-}
-
-console.log(stringToArray());
+console.log(
+  (function removeEveryOther(arr = ["Hello", "Goodbye", "Hello Again"]) {
+    return arr.filter(
+      (x,i)=>(i+1)%2
+    )
+  })()
+);
