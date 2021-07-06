834c548bc16c6437d7f95989b8dd2203d00e286e
idembele70 <idembele70@gmail.com>
2021-07-07 01:08:29 +0200

    periodisLate

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
-function orderOperations() {
-  return (2 + 2) * (2 + 2) * 2;
+function periodIsLate(
+  last = new Date(2016, 6, 13),
+  today = new Date(2016, 7, 16),
+  cycleLength = 35
+) {
+  const timeTomili = today.getTime() - last.getTime();
+  const time = timeTomili / 1000 / 60 / 60 / 24;
+  return time > cycleLength
 }
-console.log(orderOperations())
-console.log(" ".charCodeAt(0))
+
+console.log(periodIsLate());
