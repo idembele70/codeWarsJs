80f48b54fc8be30966b93cda9ed2dd48829ac92f
idembele70 <idembele70@gmail.com>
2021-07-08 00:50:14 +0200

    numberToPower

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,4 @@
-function periodIsLate(
-  last = new Date(2016, 6, 13),
-  today = new Date(2016, 7, 16),
-  cycleLength = 35
-) {
-  const timeTomili = today.getTime() - last.getTime();
-  const time = timeTomili / 1000 / 60 / 60 / 24;
-  return time > cycleLength
+function numberToPower(number = 4, power = 2) {
+  return Math.pow(number, power);
 }
-
-console.log(periodIsLate());
+console.log(numberToPower())
