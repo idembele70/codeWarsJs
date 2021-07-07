206da66bbb6a2ecbdb241546d34f4845b7fac90b
idembele70 <idembele70@gmail.com>
2021-07-08 00:55:13 +0200

    avec while

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 function numberToPower(number = 4, power = 2) {
-  return [...Array(power)].reduce(
-    (acc,curr)=>acc*=curr || number , 1
-  );
+ let i =0, res = 1
+  while (i < power) {
+   res*= number
+   i++
+ }
+ return res
 }
 console.log(numberToPower());
