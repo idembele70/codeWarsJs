88f8ce2d2b443fd82053c73228f47ce67b30263c
idembele70 <idembele70@gmail.com>
2021-07-11 01:13:53 +0200

    [main] replaceDots func

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,4 @@
-console.log(function countBy(x=2,n=5) {
-  const res = [];
-  for (let i = 1; i <= n; i++) {
-    res.push(i*x)
-  }
-  return res
-}())
+function replaceDots(str="one.two.three") {
+  return str.replace(/\./g,'-')
+}
+console.log(replaceDots()
