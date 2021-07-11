75e6b080899c4e5d3191594521c77186c712df0d
idembele70 <idembele70@gmail.com>
2021-07-12 00:46:34 +0200

    en cours

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,34 @@
-function replaceDots(str="one.two.three") {
-  return str.replace(/\./g,'-')
-}
-console.log(replaceDots()
+console.log(
+  (function first(arr = ["a", "b", "c", "d", "e"], n = 3) {
+    const alphabet =  Array.from([
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+    ]);
+    if (n == 0) return [];
+    return alphabet.splice(0, n);
+  })()
+);
