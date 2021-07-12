18724a7717c235c6af853b5d6307e673aca6ad77
idembele70 <idembele70@gmail.com>
2021-07-13 00:57:25 +0200

    number tostring

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,3 @@
-console.log(
-  (function first(arr = ["a", "b", "c", "d", "e"], n = 3) {
-    const alphabet =  Array.from([
-      "a",
-      "b",
-      "c",
-      "d",
-      "e",
-      "f",
-      "g",
-      "h",
-      "i",
-      "j",
-      "k",
-      "l",
-      "m",
-      "n",
-      "o",
-      "p",
-      "q",
-      "r",
-      "s",
-      "t",
-      "u",
-      "v",
-      "w",
-      "x",
-      "y",
-      "z",
-    ]);
-    if (n == 0) return [];
-    return alphabet.splice(0, n);
-  })()
-);
+const a = ()=> 123 .toString()
+
+console.log(123["toString"]())
