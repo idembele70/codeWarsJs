7e7f757df1bc5daad9a4b5a94b071a0c08978eb5
idembele70 <idembele70@gmail.com>
2021-07-14 00:42:51 +0200

    isLeapYear

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
-const a = ()=> 123 .toString()
 
-console.log(123["toString"]())
+function isLeapYear(year = 2000) {
+  return year % 400==0 || year % 4==0  && year % 100 != 0}
+
+console.log(isLeapYear());
+
+console.log(
+  1984 % 100,
+  1984 % 4,
+  1984 % 400,
+  true
+)
+console.log(
+  1234 % 100,
+  1234 % 4,
+  1234 % 400, false
+)
