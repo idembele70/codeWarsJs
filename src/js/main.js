7fc7ac487a072117de980a2f90c73a61cdce2f61
idembele70 <idembele70@gmail.com>
2021-07-15 00:03:47 +0200

    [main] matrix function matrix qui permet de remplacer les valeurs en fonction de l'index

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,17 @@
-console.log(
-  function getDivisorsCnt(n = 10) {
-return [...Array(n).keys()].map(
-  x=>{
-    if(!(n%(x+1))) return x+1;
-    return ""
-  }
-).filter(el=>el!="").length
-}()
-);
+console.log(function matrix(
+  array = [
+    [-1,  4, -5, -9,  3 ],
+    [ 6, -4, -7,  4, -5 ],
+    [ 3,  5,  0, -9, -1 ],
+    [ 1,  5, -7, -8, -9 ],
+    [-3,  2,  1, -5,  6 ]
+  ]
+) {
+for (let i = 0; i < array.length; i++) {
+  const arrayLine = array[i];
+  const currentNumber= arrayLine[i] < 0 ? 0 : 1
+  arrayLine.splice(i,1,currentNumber)
+}
+return array
+}());
+
