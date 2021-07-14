318609d5352821ac68eb13db2d8184523b6b86ec
idembele70 <idembele70@gmail.com>
2021-07-14 23:35:34 +0200

    avec arrays

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,10 @@
 console.log(
   function getDivisorsCnt(n = 10) {
-  let compteur = 0;
-  for (let i = 1; i < n / 2; i++) {
-    n % i == 0 ? compteur++ : null;
+return [...Array(n).keys()].map(
+  x=>{
+    if(!(n%(x+1))) return x+1;
+    return ""
   }
-  return compteur+=2
+).filter(el=>el!="").length
 }()
 );
