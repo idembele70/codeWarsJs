84dff52b202b58aa3c20899377d180e823aada99
idembele70 <idembele70@gmail.com>
2021-07-17 00:04:07 +0200

    [main] nthEven find the nth Even Number

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,6 @@
-function generateRange(min=1,max=10,step=3) {
-  const arr = []
- for (let i = min; i <= max; i+=step) {
-   arr.push(i)
-   
- }
- return arr
-}
-
-console.log(generateRange())
+console.log(
+  (function nthEven(n=100) {
+    return 2*(n-1)
+  })()
+);
 
