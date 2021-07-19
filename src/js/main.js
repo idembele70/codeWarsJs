fb66fc3664e779488760436d8b9c2075491d9c16
idembele70 <idembele70@gmail.com>
2021-07-19 23:12:02 +0200

    [main] fizzbuzz return if a number is fizzbuzz or fizz or buzz

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
-console.log(
-  function TempleStrings(obj="Animals",feature="Good") {
-    return `${obj} are ${feature}`
-  }()
-)
+function fizzbuzz(n = 3) {
+  return [...Array(n).keys()].map((number) => {
+    switch (true) {
+      case !((number+1) % 3) && !((number+1) % 5):
+        return (number = "FizzBuzz");
+      case !((number+1) % 3):
+        return (number = "Fizz");
+      case !((number+1) % 5):
+        return (number = "Buzz");
+      default:
+        return ++number;
+    }
+  });
+}
+console.log(fizzbuzz());
+
+
