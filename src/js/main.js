e1b259fd157e8a710d80036f4dcf8e3dd384aee8
idembele70 <idembele70@gmail.com>
2021-07-19 05:57:36 +0200

    [main] TempleStrings function that use template string

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
-function isDivideBy(number=-12,a=2,b=-6) {
-  return !(number%a) && !( number%b)
-}
-console.log(isDivideBy())
+console.log(
+  function TempleStrings(obj="Animals",feature="Good") {
+    return `${obj} are ${feature}`
+  }()
+)
