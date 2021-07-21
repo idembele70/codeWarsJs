1587d2e98763031e7bbf9bceca4fbdede34ff10e
idembele70 <idembele70@gmail.com>
2021-07-21 06:26:18 +0200

    [main] mergeArrays using destructuring filter and sort to resolve the problem

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
-console.log(function mergeArrays(arr1=[1,2,3,4,4], arr2=[5,6,7,8]) {
-  return [...new Set(arr1.concat(arr2).sort(
-    (a,b)=>a-b
-  ))]
-}())
-
-[main] mergeArrays using set concat & sort to resolve the problem
+console.log(
+  (function mergeArrays(arr1 = [1, 2, 3, 4, 4], arr2 = [5, 6, 7, 8]) {
+    return [...arr1,...arr2].filter(
+      (x,i,arr)=>arr.indexOf(x) === i 
+    ).sort((a,b)=>a-b)
+  })()
+);
+/*
+[main] mergeArrays using destructuring filter and sort to resolve the problem
+*/
