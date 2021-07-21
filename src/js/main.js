3dc4a856af1131cf6e0982badb0b72961f3b773f
idembele70 <idembele70@gmail.com>
2021-07-21 06:31:22 +0200

    [main] mergeArrays using for and sort to resolve the problem

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,9 @@
 console.log(
   (function mergeArrays(arr1 = [1, 2, 3, 4, 4], arr2 = [5, 6, 7, 8]) {
-    return [...arr1,...arr2].filter(
-      (x,i,arr)=>arr.indexOf(x) === i 
-    ).sort((a,b)=>a-b)
+  const arrays = [...arr1,...arr2], newArray = []
+  for (const number of arrays) {
+    if (newArray.indexOf(number) === -1)newArray.push(number);
+  }
+  return newArray.sort((a,b)=>a-b) 
   })()
 );
-/*
-[main] mergeArrays using destructuring filter and sort to resolve the problem
-*/
