5022a1461186f9bbe88bde5bb9cf02e5e7f118e8
idembele70 <idembele70@gmail.com>
2021-07-22 06:55:30 +0200

    [main] isVow return vowel char

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,7 @@
-console.log(
-  (function mergeArrays(arr1 = [1, 2, 3, 4, 4], arr2 = [5, 6, 7, 8]) {
-  const arrays = [...arr1,...arr2], newArray = []
-  for (const number of arrays) {
-    if (newArray.indexOf(number) === -1)newArray.push(number);
-  }
-  return newArray.sort((a,b)=>a-b) 
-  })()
-);
+function isVow(a=[118,117,120,121,117,98,122,97,120,106,104,116,113,114,113,120,106]) {
+  const char = (number)=> String.fromCharCode(number);
+  return a.map(x=>/[aeiou]/.test(char(x)) ? char(x) : x) 
+}
+
+console.log(isVow())
+
