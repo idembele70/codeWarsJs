198c9017cd7275904e511dfb72b1e1704d4097f7
idembele70 <idembele70@gmail.com>
2021-07-22 07:01:00 +0200

    [main] isVow returning vovel form their char code

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 function isVow(a=[118,117,120,121,117,98,122,97,120,106,104,116,113,114,113,120,106]) {
-  const char = (number)=> String.fromCharCode(number);
-  return a.map(x=>/[aeiou]/.test(char(x)) ? char(x) : x) 
+  for (let i = 0; i < a.length; i++) {
+    const currentChar = String.fromCharCode(a[i])
+    currentChar.match(/a|e|i|o|u/) ? a[i]=currentChar : 0
+  }
+  return a
 }
 
 console.log(isVow())
