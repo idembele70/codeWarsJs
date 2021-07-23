e6ca558323e600f019d1e967f09ef1c9bff1af55
idembele70 <idembele70@gmail.com>
2021-07-23 06:44:21 +0200

    sort array

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,5 @@
-function isVow(a=[118,117,120,121,117,98,122,97,120,106,104,116,113,114,113,120,106]) {
-  for (let i = 0; i < a.length; i++) {
-    const currentChar = String.fromCharCode(a[i])
-    currentChar.match(/a|e|i|o|u/) ? a[i]=currentChar : 0
+console.log(
+  function sortme(names=["one", "two", "three"]) {
+    return names.sort()
   }
-  return a
-}
-
-console.log(isVow())
-
+)
