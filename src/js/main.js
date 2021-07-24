95ec39a4e438d944e08f9027b08254607bbf54fc
idembele70 <idembele70@gmail.com>
2021-07-24 17:33:06 +0200

    [sorttwo] whit split sort and join

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
-console.log(
-  function sortme(names=["one", "two", "three"]) {
-    return names.sort()
-  }
-)
+function twoSort(s=["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]) {
+return s.sort()[0].split('').join`***`
+}
+
+console.log(twoSort())
+
