ac511b7471aecb6cd16c80041cc73c67582b3fba
idembele70 <idembele70@gmail.com>
2021-07-26 00:04:40 +0200

    [powerOfTwo] utilisation de array from

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,6 @@
 console.log(
   function powerOfTwo(n=4) {
-    const result = []
-    for (let i = 0; i <= n; i++) {
-      result.push(2**i)
-    }
-    return result
+  return Array.from({length : ++n},(v,i)=>Math.pow(2,i))
   }()
 )
+
