ef9b5e3940ad25a18a97a54645e20a7e5e9232dc
idembele70 <idembele70@gmail.com>
2021-07-28 23:15:20 +0200

    [isDisivibleArray] avec for

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,10 @@
 function divisibleBy(numbers=[1,2,3,4,5,6],divisor=2) {
-  return numbers.filter(number=>!(number%divisor))
+  const isDisivibleArray = []
+  for (const number of numbers) {
+    if(number%divisor === 0) isDisivibleArray.unshift(number)
+  }
+  return isDisivibleArray.reverse()
 }
 
 console.log(divisibleBy())
+
