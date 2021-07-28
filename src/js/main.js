855482a165e541df23f2ff45fb08dd3874f66ed6
idembele70 <idembele70@gmail.com>
2021-07-28 13:29:36 +0200

    [sumOfIntegersInString] with rest split and reduce

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 console.log(
   function sumOfIntegersInString(s="The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog") {
-    return [...s.replace(/[^0-9]/g,'')].reduce(
+    return [...s.split(/[^0-9]/g)].reduce(
       (acc,cur)=>acc+=+cur,0
     )
   }()
