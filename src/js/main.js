ed00bc549eb7ed1a3461e8f9d5eb3ae835cc4133
idembele70 <idembele70@gmail.com>
2021-07-29 07:01:14 +0200

    exo

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,5 @@
 function divisibleBy(numbers=[1,2,3,4,5,6],divisor=2) {
-  const isDisivibleArray = []
-  for (const number of numbers) {
-    if(number%divisor === 0) isDisivibleArray.unshift(number)
-  }
-  return isDisivibleArray.reverse()
+  return [...numbers.join('').replace(/./g,(v)=>v%divisor === 0 ? v : "")].map(Number)
 }
 
 console.log(divisibleBy())
