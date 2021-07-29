52a8026e51f7cf2df40b488f4e22fe8b78761ed1
idembele70 <idembele70@gmail.com>
2021-07-29 07:19:31 +0200

    [stringy] for iteration

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,10 @@
 console.log(
   function stringy(size=3) {
-    return [...Array(size)].map(
-      (value,idx)=>!(idx%2) ? 1 : 0
-      ).join``
+    let binaryString =""
+  for (let i = 0; i < size; i++) {
+   binaryString += i % 2 === 0 ? "1" : "0" 
+  }
+  return binaryString
   }()
 )
 
-//[stringy] Array  function
