271232f7a1e967996f7d40c914ac43c2d167e5eb
idembele70 <idembele70@gmail.com>
2021-07-29 07:12:42 +0200

    [stringy] array from function

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
-function divisibleBy(numbers=[1,2,3,4,5,6],divisor=2) {
-  return [...numbers.join('').replace(/./g,(v)=>v%divisor === 0 ? v : "")].map(Number)
-}
-
-console.log(divisibleBy())
+console.log(
+  function stringy(size=3) {
+    return Array.from({length : size}, (v,i)=>i%2===0 ? 1 : 0).join``
+  }()
+)
 
