6a08324e2acc19eb776a0010a596aac82153da39
idembele70 <idembele70@gmail.com>
2021-07-30 07:00:51 +0200

    [alphabetWar] using spllit ternary

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,22 @@
-console.log(
-  function stringy(size=3) {
-    let binaryString =""
-  for (let i = 0; i < size; i++) {
-   binaryString += i % 2 === 0 ? "1" : "0" 
-  }
-  return binaryString
-  }()
-)
+function alphabetWar(fight = "z") {
+  const leftSide = [0,"s", "b", "p", "w"];
+  const rightSide = [0,"z", "d", "q", "m"];
+  let leftSideSum = 0,
+    rightSideSum = 0;
+
+  fight.split('').map((letter) =>
+    leftSide.indexOf(letter) !== -1
+      ? (leftSideSum += leftSide.indexOf(letter)) : 
+      rightSide.indexOf(letter) !== -1 ?
+       (rightSideSum += rightSide.indexOf(letter))
+       : ""
+  );
+  return leftSideSum > rightSideSum
+  ? "Left side wins!"
+  : leftSideSum === rightSideSum
+  ? "Let's fight again!"
+  : "Right side wins!";
+}
+
+console.log(alphabetWar());
 
