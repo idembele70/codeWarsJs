b633bfc0ac8ec92945bbf6d96a7f53b5f114eecf
idembele70 <idembele70@gmail.com>
2021-08-01 15:03:45 +0200

    [gooseFilter] using forof and forin

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,35 @@
 console.log(
-  function gooseFilter(birds=["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]) {
-    const geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
-    const notGesse = [];
-     birds.forEach(
-      Onebird=> geese.indexOf(Onebird) === -1 ? notGesse.push(Onebird) : null
-    )
-    return notGesse
-  }()
+  (function gooseFilter(
+    birds = [
+      "Mallard",
+      "Hook Bill",
+      "African",
+      "Crested",
+      "Pilgrim",
+      "Toulouse",
+      "Blue Swedish",
+    ]
+  ) {
+    const geese = [
+      "African",
+      "Roman Tufted",
+      "Toulouse",
+      "Pilgrim",
+      "Steinbacher",
+    ];
+    const notGeese = [];
+    for (const bird of birds) {
+      let isNotGeese = true
+      for (const key in geese) {
+        if (geese[key] == bird ) {
+          isNotGeese = false
+          break
+        }
+      }
+      if(isNotGeese) notGeese.push(bird)
+    }
+    return notGeese
+  })()
 );
 
-// [gooseFilter] using foreach and indexof push
+
