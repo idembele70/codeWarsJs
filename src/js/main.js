15b12281e79a6605e90a50699fbe7ceec84e93a4
idembele70 <idembele70@gmail.com>
2021-08-01 14:54:05 +0200

    [gooseFilter] using filter and includes

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,10 @@
-function uefaeuro2016(teams = ["Germany", "Ukraine"], scores = [2, 0]) {
-  const homeTeam = teams[0],
-    awayTeam = teams[1];
-  const homeScore = scores[0],
-    awayScore = scores[1];
-  let result = "";
-  if (homeScore === awayScore) {
-    result = "teams played draw.";
-  } else {
-    if (homeScore > awayScore) result += homeTeam;
-    else result += awayTeam;
-    result += " won!";
-  }
-  return `At match ${homeTeam} - ${awayTeam}, ` + result;
-}
-console.log(uefaeuro2016());
+console.log(
+  function gooseFilter(birds=["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]) {
+    const geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return birds.filter(
+      bird=>!geese.includes(bird)
+    )
+  }()
+);
 
+// [gooseFilter] using filter and includes
