174b2bcbc2e8f652bd541202b0a94fc47dd9efe6
idembele70 <idembele70@gmail.com>
2021-08-02 07:02:20 +0200

    [toBinary] toString with parseInt

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 function toBinary(n=5) {
-  return +n.toString(2)
+  return parseInt(n.toString(2))
 }
 console.log(toBinary());
 
 /*
-[toBinary] toString with +
+[toBinary] toString with parseInt
 */
