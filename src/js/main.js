7ca42a25e8cb925428139c3a5e54d95ad033c394
idembele70 <idembele70@gmail.com>
2021-08-02 07:01:46 +0200

    [toBinary] toString with +

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,8 @@
-console.log(
-  (function gooseFilter(
-    birds = [
-      "Mallard",
-      "Hook Bill",
-      "African",
-      "Crested",
-      "Pilgrim",
-      "Toulouse",
-      "Blue Swedish",
-    ]
-  ) {
-    const geese = [
-      "African",
-      "Roman Tufted",
-      "Toulouse",
-      "Pilgrim",
-      "Steinbacher",
-    ];
-    const notGeese = [];
-    for (const bird of birds) {
-      let isNotGeese = true
-      for (const key in geese) {
-        if (geese[key] == bird ) {
-          isNotGeese = false
-          break
-        }
-      }
-      if(isNotGeese) notGeese.push(bird)
-    }
-    return notGeese
-  })()
-);
+function toBinary(n=5) {
+  return +n.toString(2)
+}
+console.log(toBinary());
 
-
+/*
+[toBinary] toString with +
+*/
