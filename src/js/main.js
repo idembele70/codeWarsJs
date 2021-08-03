07b928687e78ba6fdcae1c494f054922705f4169
idembele70 <idembele70@gmail.com>
2021-08-03 06:58:54 +0200

    [pipeFix] using fill Array map

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,11 @@
 console.log(
   (function pipeFix(numbers = [6, 9]) {
-    const max = numbers[numbers.length - 1];
-    const min = numbers[0];
-    const arrayOfPipeFix = []
-    for (let i = min; i <= max; i++) {
-      if(i <= max)arrayOfPipeFix.push(i)
-    }
-    return arrayOfPipeFix
+    const pipeFixLength = numbers[numbers.length - 1] - numbers[0] + 1;
+    const minNumbers = numbers[0]
+    return Array(pipeFixLength).fill(minNumbers).map(
+      (number,idx)=>number+=idx
+    );
   })()
 );
 
-// [pipeFix] using for loop
+// [pipeFix] using fill Array map
