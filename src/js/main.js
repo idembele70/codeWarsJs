acad8e49972a97fc4c688375f7f2eaf2cf339284
idembele70 <idembele70@gmail.com>
2021-08-06 06:51:51 +0200

    [multiply] function that multiply two number

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 console.log(
-  function all(arr=[1,2,3,4,5,10],fun= function(v){return v<9}) {
-  return arr.filter(fun).length === arr.length  
+  function multiply(numberOne, numberTwo) {
+    return numberOne * numberTwo
   }()
-);
-
+)
