58f9430e5017c2accf7b2bc5688070ced69e3c3e
idembele70 <idembele70@gmail.com>
2021-08-07 12:04:21 +0200

    [multipleOfIndex] usign filter and indexof

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 console.log(
-  function multiply(numberOne, numberTwo) {
-    return numberOne * numberTwo
+  function mulpileOfIndex(array= [68, -1, 1, -7, 10, 10])
+  {
+    return array.filter(
+      (number,i)=>number % array.indexOf(number,i) === 0
+    )
   }()
 )
+// [multipleOfIndex] usign filter and indexof
