14dca57350aa4525407eebd61906ed53ef8de24a
idembele70 <idembele70@gmail.com>
2021-08-07 12:04:57 +0200

    [multipleOfIndex] using only filter

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,8 @@ console.log(
   function mulpileOfIndex(array= [68, -1, 1, -7, 10, 10])
   {
     return array.filter(
-      (number,i)=>number % array.indexOf(number,i) === 0
+      (number,i)=>number % i === 0
     )
   }()
 )
-// [multipleOfIndex] usign filter and indexof
+// [multipleOfIndex] using only filter
