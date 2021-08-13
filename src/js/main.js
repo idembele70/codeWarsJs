39777fa17b9126741284e0eb1b6e50889cea6645
idembele70 <idembele70@gmail.com>
2021-08-13 07:04:43 +0200

    encour

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,16 @@
-function sum(digits="3433") {
-  let tabs = []
-  return digits.split('').reduce(
-    (acc,cur)=>{
-      tabs.push()
-      acc+ +cur,0
+function sum(digits="1056653001858539") {
+  const tabs = []
+ let sum  = 0
+ if(digits.length) return digits
+ digits.split('').forEach(
+    (num)=>{
+      tabs.push(num)
+      sum+= parseInt(num)
     }
   )
+  return `${tabs.join` + `} = ${sum}`
 }
 console.log(
   sum()
 )
-//[sum] encour
+// encour
