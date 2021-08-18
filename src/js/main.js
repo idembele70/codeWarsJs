8a4db626178a861a446a56076cd62acbd80ab3ab
idembele70 <idembele70@gmail.com>
2021-08-18 07:21:17 +0200

    [solve] using return

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,7 @@
-String.prototype.toAlternatingCase = function () {
-  let alternatedString = ""
-  for (let i = 0; i < this.length; i++) {
-    const currentLetter = this.charAt(i);
-    if(currentLetter === currentLetter.toUpperCase())
-    alternatedString += currentLetter.toLowerCase()
-    else
-    alternatedString += currentLetter.toUpperCase()
-  }
-  return alternatedString
-}
+console.log(
+  function solve(x=9999, y=11) {
+  return  x / y
+  }()
+);
 
-console.log("hello worlD".toAlternatingCase());
-
-// [toAlternatingCase] using for
+// [solve] using return
