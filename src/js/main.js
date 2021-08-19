aa59416801934475c2d6661371f07763269950b7
idembele70 <idembele70@gmail.com>
2021-08-19 23:39:41 +0200

    [hexToDec] using parseint

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
-const {divide} = require("ramda")
-console.log(
-  function solve(x=9999, y=11) {
-  divide(x,y)
-  }()
-);
-// [solve] using return
+function hexToDec(hexString="a") {
+  return parseInt(hexString,16)
+}
+
+console.log(hexToDec());
+// [hexToDec] using parseint
