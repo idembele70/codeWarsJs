d4042a53ae972d35c44b203f0eb212418fc31838
idembele70 <idembele70@gmail.com>
2021-08-24 23:41:33 +0200

    [getPlanet] using switch and return

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,25 @@
-console.log(
-  function preFizz(n=2) {
-    return Array.from({length : n}, (_value, idx)=>++idx)
-  }()
-);
-// [preFizz] using array.from 
+function getPlanetName(id=2) {
+
+    switch(id){
+      case 1:
+        return 'Mercury'
+      case 2:
+        return 'Venus'
+      case 3:
+        return 'Earth'
+      case 4:
+        return 'Mars'
+      case 5:
+        return 'Jupiter'
+      case 6:
+        return 'Saturn'
+      case 7:
+        return 'Uranus'
+      case 8:
+        return 'Neptune'
+    }
+}
+console.log(getPlanetName())
+// [getPlanet] using switch and return  
 
 
