d5f2c8b183737bb9601d51a625c4126b30534ebf
idembele70 <idembele70@gmail.com>
2021-08-24 23:44:25 +0200

    [getPlanet] object

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,16 @@
 function getPlanetName(id = 2) {
-  const tabs =  ["","Mercury", 'Venus',
-    'Earth',
-    'Mars',
-    'Jupiter',
-    'Saturn',
-    'Uranus',
-    'Neptune']
-  return tabs[id]
+  const planet =  {
+    1 : "Mercury",
+    2 : "Venus",
+    3 : "Earth",
+    4 : "Mars",
+    4 : "Jupiter",
+    5 : "Saturn",
+    6 : "Uranus",
+    7 : "Neptune"}
+  return planet[id]
 }
 console.log(getPlanetName())
-// [getPlanet] Array
+// [getPlanet] object
 
 
