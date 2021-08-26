65601da972c5c19fe89b7632bed4a79fdf73e629
idembele70 <idembele70@gmail.com>
2021-08-26 14:53:49 +0200

    [shortenToDate] using split slice join

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,5 @@
-console.log(
-  function colorOf(r=255,g=0,b=0){
-    const red = (r.toString(16).length === 1 ? "0" : "") + r.toString(16)
-    const green = (g.toString(16).length === 1 ? "0" :"") + g.toString(16)
-    const blue =  (b.toString(16).length === 1 ? "0" :"") + b.toString(16)
-    return `#${red}${green}${blue}`
-  }()
-);
-// [colorOf] using toString
-
-
+function shortenToDate(longDate="Friday May 2, 9am") {
+  return longDate.split(',').slice(0,-1).join(" ")
+}
+console.log(shortenToDate());
+// [shortenToDate] using split slice join
