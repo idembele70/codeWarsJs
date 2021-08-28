e1c8494fa673be9557fd44544d4748131356e17e
idembele70 <idembele70@gmail.com>
2021-08-28 14:13:15 +0200

    [joinString] using template string

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 console.log(
   function joinString(string1="string1", string2="string2"){
-    return string1.toString().concat(" ", string2)
+    return `${string1} ${string2}`
   }()
 );
-// [joinString] using concat
+// [joinString] using template string
