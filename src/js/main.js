08f7c6ac40af8883a87b9d239f961f19f28c9b33
idembele70 <idembele70@gmail.com>
2021-08-31 22:02:53 +0200

    [blackBox] using blackBox

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
-function filterNumbers(str="aa1bb2cc3dd") {
-  return str.replace(/\d/g,"")
-}
-console.log(filterNumbers());
-// [filterNumbers] using template string
+console.log(
+  function blackBox() {
+    return "It's clamped shut!"
+  }()
+);
+// [blackBox] using blackBox
+
