cd3ffd36a8d4ab407b32808ebdfd5ef972586a4c
idembele70 <idembele70@gmail.com>
2021-09-04 22:35:09 +0200

    [digitize] W/ rest

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,5 @@
-console.log(
-  function findMultiples(integer=5,limit=25) {
-    const length = limit / integer
-    const ArrayLength = length.toString().split('.')[0]
-    return [...Array(+ArrayLength)].map(
-      (value,idx)=>integer * idx + integer
-    )
-  }()
-  );
-// [findMultiples] W/ Array & destructure
+function digitize(n=35231) {
+  return [...n.toString()].reverse().map(Number)
+}
+ console.log(digitize()); ;
+// [digitize] W/ rest 
