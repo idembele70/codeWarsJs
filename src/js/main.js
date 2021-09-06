3548f5ecd21bb5afa8fe68b5caf26a0834601c24
idembele70 <idembele70@gmail.com>
2021-09-06 22:34:30 +0200

    [calculator] usign typeof switch

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,21 @@
 console.log(
-  function calculator(a=1,b=2,sign="+") {
-  if(typeof a === "string" || typeof b === "string" || !["-","+","*",'/'].includes(sign) )
-  return "unknown value"
-  else 
-  return eval(`${a}${sign}${b}`)  
-} ()
+  function calculator(a = 1, b = 2, sign = "+") {
+    if (typeof a === "string" || typeof b === "string")
+      return "unknown value"
+    else
+      switch (sign) {
+        case "+":
+          return a + b
+        case "-":
+          return a - b
+        case "*":
+          return a * b
+        case "/":
+          return a / b
+        default:
+          return "unknown value"
+      }
+  }()
 );
 
-//  [calculator] usign typeof includes and eval
+//  [calculator] usign typeof switch
