baf7cbbbe9b8ae78c9b2bffa13a408396ab8ccae
idembele70 <idembele70@gmail.com>
2021-09-08 12:12:09 +0200

    [xor] using xor operator

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,6 @@
-
-
-function arrayMadness(a=[4,5,6],b=[1,2,4]) {
-let sumOfA = 0
-let sumOfB = 0
-for (const number of a) {
-  sumOfA += Math.pow(number,2)
-}
-for (const number of b) {
-  sumOfB += Math.pow(number,3)
-}
-return sumOfA > sumOfB
-}
-
-console.log(arrayMadness());
-
-// [reducer] usign for of
+console.log(
+  function xor(a = true, b = true) {
+    return !!(a ^ b)
+  }()
+);
+// [xor] using xor operator
