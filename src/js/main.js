6d119710ff017e0f07831752495437bf05f0bcdd
idembele70 <idembele70@gmail.com>
2021-09-09 10:24:24 +0200

    [isUpperCase] using if concat & match

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
-console.log(
-  function xor(a = true, b = true) {
-    return !!(a ^ b)
-  }()
-);
-// [xor] using xor operator
+String.prototype.isUpperCase = function() {
+  if(this.concat().match(/[a-z]/g)) return false
+
+  return this.concat() ?  true :  "Must define the prototype isUpperCase"
+}
+console.log("HELLO I AM DONALD".isUpperCase())
+// [isUpperCase] using if concat & match
