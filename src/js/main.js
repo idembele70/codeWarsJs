f03c1728fdb886e8b12e02bd0e5a49746013edd5
idembele70 <idembele70@gmail.com>
2021-09-11 11:12:02 +0200

    [between] using Array & map functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,8 @@
-String.prototype.isUpperCase = function () {
-  for (let i = 0; i < this.length; i++) {
-    const currentChar = this[i]
-    console.log(currentChar);
-    if (!currentChar.match(/[a-z]/)) continue
-    else return false
-  }
-  return !this.length ? "Must define the prototype isUpperCase" : true
-}
-console.log("HELLO I AM DONALD".isUpperCase())
-// [isUpperCase] using if for & match
+console.log(
+  function between(a=1,b=4) {
+    return Array(b+1-a).fill(a).map(
+      (number,idx)=>number+idx
+    )
+  }()
+);
+// [between] using Array & map functions
