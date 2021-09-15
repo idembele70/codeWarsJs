92f579eacdb5b5b454ebcc8b76799ab8eccb8ec8
idembele70 <idembele70@gmail.com>
2021-09-15 08:11:31 +0200

    [strCount] using regex & match

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 console.log(
-  function strCount(str="hello",letter="p") {
-    return [...str].filter(strLetter=> strLetter === letter).length
+  function strCount(str="hello",letter="a") {
+    const letterFinder = new RegExp(letter,"g")
+    const arrayOfFind = str.match(letterFinder)
+    return arrayOfFind ? arrayOfFind.length : 0
   }()
 );
-// [strCount] using filter
+// [strCount] using regex & match
