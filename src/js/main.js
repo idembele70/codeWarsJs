6a927fac153b1e53d764911e10bfa4204d0286e4
idembele70 <idembele70@gmail.com>
2021-09-17 23:35:13 +0200

    [shortcut] using replace and regex

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
-function usdcny(usd=15) {
- return 101.25 / 15
-}
-console.log(usdcny());
-// [usdcny] using toFixed and template string
+console.log(
+  function shortcut(string="codewars") {
+    return string.replace(/[aeuio]/g, "")
+  }()
+);
+// [shortcut] using replace and regex
