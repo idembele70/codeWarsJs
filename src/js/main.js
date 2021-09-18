dd2ebbaaebc4eb586b2ec7c819dd2f5a89e0a209
idembele70 <idembele70@gmail.com>
2021-09-18 07:25:38 +0200

    [binToDec] for iteration

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,14 @@
 function binToDec(bin=10) {
-  return parseInt(bin, 2)
+   bin = bin.toString()
+  const convertedNumbers = []
+  for (let i = bin.length; i > 0; i--) {
+    const currentNumber = bin[i-1]
+    const convertedNumber = currentNumber * Math.pow(2,bin.length - i)
+   convertedNumbers.push(convertedNumber)
+  }
+  return convertedNumbers.reduce(
+    (sum,currentVal)=>sum+currentVal
+  )
 }
 console.log(binToDec());
-// [binToDec] parseInt with his radix
+// [binToDec] for iteration
