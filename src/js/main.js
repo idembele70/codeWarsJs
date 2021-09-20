8d50f38149d7e4b0cf4ceb5f95cbf01c9a85fc4c
idembele70 <idembele70@gmail.com>
2021-09-20 22:47:31 +0200

    [add] using func in func

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
-console.log(
-  function billboard(name = "Jeong-Ho Aristotelis", price = 30) {
-    return Array.from({ length: name.length }, (v) => v = price || 30).reduce((acc, c) => acc + c, 0)
-  }()
-);
-// [billboard] using Array FROM and REDUCE
+function add(n=1) {
+  return (addToN)=>n+addToN
+}
+const addOne = add(4)
+console.log(addOne(2));
+// [add] using func in func
