feca5684cd96d8696d6b743e7c04fe8c96290601
idembele70 <idembele70@gmail.com>
2021-09-23 15:01:47 +0200

    [_if] using ternary

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 console.log(
   function _if(bool = false, func1 = () => console["log"](true), func2 = () => console["log"](false)) {
-    if (bool) return func1()
-    return func2()
+    return bool ? func1() : func2()
   }()
 );
-// [_if] using if condition
+// [_if] using ternary
