d1426c613502388cc17310818ea884754d0b9803
idembele70 <idembele70@gmail.com>
2021-09-27 22:19:46 +0200

    [name] using regex condition replace

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,10 @@
 console.log(
   function name(name = "johN") {
-    if (name) {
-      name = name.replace(/[A-Z]/ig, (letter, idx) => idx === 0 ? letter.toUpperCase() : letter.toLowerCase()) + "!"
-    }
-    return `Hello, ${name + "!" || "World!"}`
+    if(name){ name + "!"
+    name = name.toLowerCase()
+    name = name.replace(name.charAt(0), v=>v.toUpperCase())
+     }
+    return "Hello, " + (name || "World")+"!"
   }()
 )
-// [name] using replace with regex
+// [name] using regex condition replace
