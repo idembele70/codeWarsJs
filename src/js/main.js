b4189eecff3756b4c12d121c7480e05be7a617d3
idembele70 <idembele70@gmail.com>
2021-10-01 23:51:54 +0200

    [square] using ** operator

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,6 @@
-function twoHighest(arr = [20, 17, 17,18]) {
-  const arrSorted = arr.filter(
-    (x,i,array)=>array.indexOf(x) === i 
-  ).sort(
-    (a,b)=>a-b
-  )
-  return arrSorted.slice(-2).reverse()
-}
-const result = twoHighest()
-console.log(result);
-// [twoHighest] using filter sort slice
+console.log(
+  function square(number=3) {
+    return number ** 2
+  }
+);
+// [square] using ** operator
