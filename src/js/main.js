044c1c91c224c76145412b7a01516d7435d3db8c
idembele70 <idembele70@gmail.com>
2021-10-04 22:58:37 +0200

    [decodePass] using split reduce String FromCharCode parseInt and forof iteration

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,15 @@
 console.log(
   function decodePass(passArr=['password123', 'admin', 'admin1'], bin="01110000 01100001 01110011 01110011 01110111 01101111 01110010 01100100 00110001 00110010 00110011") {
-    bin = bin.split(' ').map(bineary=>String.fromCharCode(parseInt(bineary,2))).join('')
-    return passArr.find(
-      pass=> bin === pass
-    ) || false
+   const binToString = bin.split(' ').reduce(
+     (acc,curr)=>acc+= String.fromCharCode(
+       parseInt(curr,2)
+     ),""
+   )
+     for (const pass of passArr) {
+     if(binToString === pass) return pass
+   } 
+   return false
   }()
 );
-// [decodePass] using map split String charcode parseInt join and find
+// [decodePass] using split reduce String FromCharCode parseInt and forof iteration
 
