3ca6e806b94774833114c8f900b4c0af86eb98df
idembele70 <idembele70@gmail.com>
2021-10-04 22:55:46 +0200

    [decodePass] using map split String charcode parseInt join and find

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,10 @@
-function htmlspecialchars(formData = "<h2>Hello World</h2>") {
-  const data = {
-    "<": "&lt;",
-    ">": "&gt;",
-    "\"": "&quot;",
-    "&": "&amp;"
-  }
-  return formData.split('').map(
-    word=>data[word] || word
-  ).join('')
-}
-console.log(htmlspecialchars());
-// [htmlspecialChars] using object split map and join
+console.log(
+  function decodePass(passArr=['password123', 'admin', 'admin1'], bin="01110000 01100001 01110011 01110011 01110111 01101111 01110010 01100100 00110001 00110010 00110011") {
+    bin = bin.split(' ').map(bineary=>String.fromCharCode(parseInt(bineary,2))).join('')
+    return passArr.find(
+      pass=> bin === pass
+    ) || false
+  }()
+);
+// [decodePass] using map split String charcode parseInt join and find
+
