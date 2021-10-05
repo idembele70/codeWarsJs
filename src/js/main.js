4589aeca334d0c0ddd85b0ac60ee3f6322790788
idembele70 <idembele70@gmail.com>
2021-10-05 22:41:01 +0200

    [connotation] trying

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,13 @@
 console.log(
-  function decodePass(passArr=['password123', 'admin', 'admin1'], bin="01110000 01100001 01110011 01110011 01110111 01101111 01110010 01100100 00110001 00110010 00110011") {
-   const binToString = bin.split(' ').reduce(
-     (acc,curr)=>acc+= String.fromCharCode(
-       parseInt(curr,2)
-     ),""
-   )
-     for (const pass of passArr) {
-     if(binToString === pass) return pass
-   } 
-   return false
+  function connotation(str="A big brown fox caught a bad bunny") {
+    let firstHalfCount = 0
+    let secondHalfCount = 0
+    str = str.replace(/\s+/g," ")
+    str.split(' ').forEach(
+      word=>word.charAt(0).toLowerCase() <= "m" ? firstHalfCount++ : secondHalfCount++
+    )
+    return firstHalfCount >= secondHalfCount
   }()
 );
-// [decodePass] using split reduce String FromCharCode parseInt and forof iteration
+// [connotation] trying
 
