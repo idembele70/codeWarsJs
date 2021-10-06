52614682d185ab8b1cfee306670181fef57cbe0c
idembele70 <idembele70@gmail.com>
2021-10-06 15:39:40 +0200

    [f] using for iteration if condition, else if type of

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,23 @@
-console.log(
-  function connotation(str="A big brown fox caught a bad bunny") {
-    let firstHalfCount = 0
-    let secondHalfCount = 0
-    str = str.replace(/\s+/g," ")
-    str.split(' ').forEach(
-      word=>word.charAt(0).toLowerCase() <= "m" ? firstHalfCount++ : secondHalfCount++
-    )
-    return firstHalfCount >= secondHalfCount
-  }()
-);
-// [connotation] trying
+function f(n = 100) {
+  let sum = 0
+  let res = ""
+  for (let i = 1; i <= n; i++) {
+    sum += i
+  }
+  if (typeof (n) === "string")
+    res = false, ", n must be a number"
+  else if (typeof (n) === "undefined")
+    res = false, "n must be a value"
+  else if (!Number.isInteger(n))
+    res = false, "n must be an integer"
+  else if (n === 0)
+    res = false, "a value of n greater than 0 is required"
+  else if (n < 0)
+    res = false, "a positive value of n is required"
+  else
+    res = sum
+  return res
+}
+console.log(f());
+// [f] using for iteration if condition, else if type of 
 
