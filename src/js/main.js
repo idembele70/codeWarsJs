5bc07be058dfdb9befaecae3f7d370ab1880ece2
idembele70 <idembele70@gmail.com>
2021-10-08 23:12:46 +0200

    [sayHello] using string template

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,7 @@
-function f(n = 100) {
-  let sum = 0
-  let res = ""
-  for (let i = 1; i <= n; i++) {
-    sum += i
+console.log(
+  function sayHello(name="Mr. Spock") {
+    return `Hello, ${name}`
   }
-  if (typeof (n) === "string")
-    res = false, ", n must be a number"
-  else if (typeof (n) === "undefined")
-    res = false, "n must be a value"
-  else if (!Number.isInteger(n))
-    res = false, "n must be an integer"
-  else if (n === 0)
-    res = false, "a value of n greater than 0 is required"
-  else if (n < 0)
-    res = false, "a positive value of n is required"
-  else
-    res = sum
-  return res
-}
-console.log(f());
-// [f] using for iteration if condition, else if type of 
+);
+// [sayHello] using string template
 
