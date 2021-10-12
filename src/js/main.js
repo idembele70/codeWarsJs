d8b959cce570a1fce63cd992edcd7523d2c0577d
idembele70 <idembele70@gmail.com>
2021-10-12 11:32:46 +0200

    [is_square] using filter sqrt toString indexOf

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
 function is_square(arr=[1,4,9,16]) {
-  if(!arr.length) return undefined
- return arr.every(
-    number=>!(Math.sqrt(number).toString().includes("."))
-  )
+  const {length} = arr
+ if(length)
+ return arr.filter(
+   number=> [...Math.sqrt(number).toString()].indexOf(".") === -1
+ ).length === length
+ else
+ return undefined
 }
 console.log(is_square());
-// [is_square] using length method, every, sqrt number toString and includes function
+// [is_square] using filter sqrt toString indexOf
 
