0e7f57e489b7ef045b081f7a587f6a77dead8b15
idembele70 <idembele70@gmail.com>
2021-10-15 13:48:32 +0200

    [stray] using Number classes filter lastIndexOf indexOf & logic and join function

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 function stray(numbers=[1,1,2]) {
-  for (const number of numbers) {
-    if(numbers.lastIndexOf(number) === numbers.indexOf(number)) return number
-  }
+  return Number(numbers.filter(
+    (number, idx)=> numbers.lastIndexOf(number) === idx && numbers.indexOf(number) === idx
+  ).join``)
 }
 console.log(stray());
-// [stray] using forof iteration lastIndexOf and indexOf iterration 
+// [stray] using Number classes filter lastIndexOf indexOf & logic and join function
