a3e9a95b9dfb5d743b9094b4dde17df6327d9bb1
idembele70 <idembele70@gmail.com>
2021-10-18 23:08:11 +0200

    [doubleChar] using replace w regex and function inside replace

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,6 @@
-function incrementer(nums=[4, 6, 7, 1, 3]) {
-  const numsIncremented = []
-  for (let index = 0; index < nums.length;) {
-    const num = nums[index] + ++index
-    numsIncremented.push(Number(...[...String(num)].slice(-1)))
-  }
-  return numsIncremented
-}
-console.log(incrementer());
-// [incrementer] using for iteration push function Number and String classes slice function
+console.log(
+  function doubleChar(str="abcd") {
+    return str.replace(/./ig,(v)=>v+v)
+  }()
+);
+// [doubleChar] using replace w regex and function inside replace
