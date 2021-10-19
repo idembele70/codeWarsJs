5d365deac40c3f14b3c68729bee21fa1cf47c0b1
idembele70 <idembele70@gmail.com>
2021-10-19 23:49:12 +0200

    [doubleChar] using let variable forof iteration string concatenation and string addition

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,9 @@ console.log(
   function doubleChar(str="abcd") {
    let strRepeated = ""
     for (const letter of str) {
-     strRepeated += `${letter}${letter}`
+     strRepeated += letter.concat(letter)
    }
    return strRepeated
   }()
 );
-// [doubleChar] using let variable forof iteration string template and string addition
+// [doubleChar] using let variable forof iteration string concatenation and string addition
