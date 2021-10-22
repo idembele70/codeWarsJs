3fd29338343083672564c79d1a016b0ec06e2343
idembele70 <idembele70@gmail.com>
2021-10-22 17:44:32 +0200

    [grasshoper] using one const variable to define multiple variable and int addition

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,6 @@ var copaDelReyGoals
 
 var totalGoals
 //to 
-const laLigaGoals = 43
-const championsLeagueGoals = 10
-const copaDelReyGoals = 5
-const totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
-// [grasshoper] using const variable and int addition
+const laLigaGoals = 43, championsLeagueGoals = 10, copaDelReyGoals = 5, 
+totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+// [grasshoper] using one const variable to define multiple variable and int addition
