bde03b58991b5e64e0f85399290088614384a750
idembele70 <idembele70@gmail.com>
2021-10-23 13:05:37 +0200

    [reverseList] using reverse function

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,6 @@
-// went from 
-var laLigaGoals
-var championsLeagueGoals
-var copaDelReyGoals
-
-var totalGoals
-//to 
-const laLigaGoals = 43, championsLeagueGoals = 10, copaDelReyGoals = 5, 
-totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
-// [grasshoper] using one const variable to define multiple variable and int addition
+console.log(
+  function reverseList(list = [1, 2, 3, 4]) {
+return list.reverse()
+  }()
+);
+// [reverseList] using reverse function
