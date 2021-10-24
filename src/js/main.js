f820f4e62b7e9b2c0c3fb7f642f59f2ed85b9aaf
idembele70 <idembele70@gmail.com>
2021-10-24 23:47:34 +0200

    [reverseList] using for iteration, const variable, push function from array

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
 console.log(
   function reverseList(list = [1, 2, 3, 4]) {
-    const { length: listLength } = list
-    return Array.from({length : listLength},(_number,idx)=>list[listLength - idx - 1])
+    const newList = []
+    for (let i = list.length; i > 0; i++) {
+      const number = list[i]
+      newList.push(number)
+    }
+    return newList
   }()
 );
-// [reverseList] using Array from destructuration and function inside array from
+// [reverseList] using for iteration, const variable, push function from array
