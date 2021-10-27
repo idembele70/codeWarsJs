c9ab72a3bb755535b47f84cc81d9198d27debaa2
idembele70 <idembele70@gmail.com>
2021-10-27 19:51:05 +0200

    [getEvenNumbers] using for iteration, Array push function

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 function getEvenNumbers(numbersArray=[1,2,3]) {
   const filtered = []
-   numbersArray.forEach(
-    number=>number % 2 ===0 && filtered.push(number)
-  )
+  for (let i = 0; i < numbersArray.length; i++) {
+    const number = numbersArray[i];
+    if(number % 2) continue
+    else filtered.push(number)
+  }
   return filtered
 }
 console.log(getEvenNumbers());
-// [getEvenNumbers] using forEach iteration
+// [getEvenNumbers] using for iteration, Array push function
