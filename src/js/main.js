95d20207761d37a7e27e91aac79e34c48bbe02ad
idembele70 <idembele70@gmail.com>
2021-10-28 11:58:29 +0200

    [solution] using for replace function

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,4 @@
-function getEvenNumbers(numbersArray=[1,2,3]) {
-  const filtered = []
-  for (let i = 0; i < numbersArray.length; i++) {
-    const number = numbersArray[i];
-    if(number % 2) continue
-    else filtered.push(number)
-  }
-  return filtered
+function solution(string="camelCasing") {
+  return string.replace(/[Z-A]/g," $&")
 }
-console.log(getEvenNumbers());
-// [getEvenNumbers] using for iteration, Array push function
+// [solution] using for replace function
