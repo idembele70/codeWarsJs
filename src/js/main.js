e97bbfdddb7f7e491cd653df58f664d1e50e5898
idembele70 <idembele70@gmail.com>
2021-11-01 15:07:42 +0100

    [solve] using split foreach match push toUpperCase ToLowerCase functions and switch condition

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,16 @@
-console.log(
-  function multiply(a, b) {
-    return a * b
+function solve(s="cODE") {
+  const lower = [], upper=[]
+   s.split('').forEach(
+    letter=>letter.match(/[a-z]/) ? lower.push(letter): upper.push(letter)
+  )
+  switch (true) {
+    case lower.length < upper.length:
+      return s.toUpperCase()
+    case lower.length > upper.length:
+      return s.toLowerCase()
+    default:
+      return s.toLowerCase()
   }
-);
-// [MULTIPLY] using RETURN
+}
+console.log(solve());
+// [solve] using split foreach match push toUpperCase ToLowerCase functions and switch condition
