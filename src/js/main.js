b34364fb0f1c2b01abe99c779b133d0806a2d731
idembele70 <idembele70@gmail.com>
2021-11-03 16:37:13 +0100

    [distinct] using const variable, includes push function Array classes

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 console.log(
   function distinct(a=[1,1,2]) {
-    return a.filter(
-      (number,idx,newArr)=>newArr.indexOf(number) === idx
-    )
+    const aFiltered = []
+    for (const number of a) {
+      if(!aFiltered.includes(number))
+      aFiltered.push(number)
+    }
+    return aFiltered
   }
 );
-// [distinct] using Array filter, indexOf function 
+// [distinct] using const variable, includes push function Array classes
