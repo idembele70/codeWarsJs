c2baeeb47c0bfdbf641e2e5998481e1531146b01
idembele70 <idembele70@gmail.com>
2021-11-04 12:30:43 +0100

    [filterString] using parseInt rest map filter Number classes isNAN and join funcitons

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 function filterString(value = "123a") {
-  return +value.replace(/\D/g,"")
+  return parseInt([...value].map(Number).filter(
+    number=>!Number.isNaN(number)
+  ).join``)
 }
 console.log(filterString());
 // [filterString] using parseInt replace function and regex
