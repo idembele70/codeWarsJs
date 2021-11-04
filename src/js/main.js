a9623b7edd6244e35ba118ce0dd386e6356d0e13
idembele70 <idembele70@gmail.com>
2021-11-04 12:34:40 +0100

    [filterString] using const variable for iteration Number classes isinteger push and join functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,12 @@
 function filterString(value = "123a") {
-  return parseInt([...value].map(Number).filter(
-    number=>!Number.isNaN(number)
-  ).join``)
+  const filteredString = []
+  for (let n = 0; n < value.length; n++) {
+    const number = +value[n]
+    if(Number.isInteger(number))
+    filteredString.push(number)
+    
+  }
+  return Number(filteredString.join``)
 }
 console.log(filterString());
-// [filterString] using parseInt replace function and regex
+// [filterString] using const variable for iteration Number classes isinteger push and join functions
