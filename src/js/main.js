87444d4f84257640674dae840046bf249a403add
idembele70 <idembele70@gmail.com>
2021-11-04 12:23:51 +0100

    [filterString] using parseInt replace function and regex

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,5 @@
-console.log(
-  function distinct(a=[1,1,2]) {
-    const aFiltered = []
-    for (const number of a) {
-      if(!aFiltered.includes(number))
-      aFiltered.push(number)
-    }
-    return aFiltered
-  }
-);
-// [distinct] using const variable, includes push function Array classes
+function filterString(value = "123a") {
+  return +value.replace(/\D/g,"")
+}
+console.log(filterString());
+// [filterString] using parseInt replace function and regex
