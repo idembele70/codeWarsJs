a1756d92a56f1a520baae359af82c9c6b4631426
idembele70 <idembele70@gmail.com>
2021-11-07 17:34:55 +0100

    [switchItUp] using using object

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,17 @@
-console.log(
-  function isDivisibe(n=3,x=1,y=1) {
-    return !(n % x) && !(n % y)
+function switchItUp(number=1) {
+  const numbers= {
+    0: "Zero",
+    1: "One",
+    2: "Two",
+    3: "Three",
+    4: "Four",
+    5: "Five",
+    6: "Six",
+    7: "Seven",
+    8: "Eight",
+    9: "Nine"
   }
-);
-// [isDisivisble] using return ternary and codition reverse with !
+  return numbers[number]
+}
+console.log(switchItUp());
+// [switchItUp] using using object
