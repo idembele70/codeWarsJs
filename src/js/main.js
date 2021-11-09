5c7ffc39154c2a2a91541f2cf3183b4cd80f86f5
idembele70 <idembele70@gmail.com>
2021-11-09 15:24:00 +0100

    [greaterThanLessThan] < sign

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,7 @@
 console.log(
-  function remainder(a = 17, b = 5) {
-    if (!(a || b)) return NaN
-    else {
-      if (a > b)
-        return a % b
-      return b % a
-    }
+  function greaterThanLessThan(a=-9,b=-8, c=-7) {
+    return a < b < c
   }()
 );
-// [remainder] using if conditions else and return and modulo operator
+
+// [greaterThanLessThan] < sign
