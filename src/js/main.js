db5962f90ac5727ef17e37e13bb7ddbb3336f4b7
idembele70 <idembele70@gmail.com>
2021-11-11 22:08:39 +0100

    [findLongest] using split function const let variable for iteration if condition and return

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,15 @@
-blackBox()
-console?.log(blackBox())
-JSopenSesame()
+function findLongest(str="The quick white fox jumped around the massive dog") {
 
-// [blackBox] guessing !
+  const strSplited = str.split(" ");
+  let longest = 0
+  
+  for (let i = 0; i < strSplited.length; i++) {
 
-/* 
-PAUL 
-*/
+    if (strSplited[i].length > longest) {
+      longest = strSplited[i].length
+    }
+    }
+    return longest
+  }
+console.log(findLongest());
+// [findLongest] using split function const let variable for iteration if condition and return
