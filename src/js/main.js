a3d5d63820b41f8c3e836f05c532926ba9caac97
idembele70 <idembele70@gmail.com>
2021-11-12 22:39:23 +0100

    [fizzbuzz] using swtich and return

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,15 @@
-function findLongest(str="The quick white fox jumped around the massive dog") {
-
-  const strSplited = str.split(" ");
-  let longest = 0
-  
-  for (let i = 0; i < strSplited.length; i++) {
-
-    if (strSplited[i].length > longest) {
-      longest = strSplited[i].length
+console.log(
+  function fizzbuzz(n=1) {
+    switch (true) {
+      case !(n % 5 && n % 3) :
+        return "FizzBuzz"
+      case n % 5 === 0:
+        return "Buzz"
+      case !(n % 3) :
+        return "Fizz"
+      default:
+        return "Not Fizz nor Buzz"
     }
-    }
-    return longest
-  }
-console.log(findLongest());
-// [findLongest] using split function const let variable for iteration if condition and return
+  }()
+);
+// [fizzbuzz] using swtich and return
