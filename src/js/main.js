9055710b2e9aaeb4401ddf77c7872c671a25a702
idembele70 <idembele70@gmail.com>
2021-11-13 23:06:17 +0100

    [generateShape] using Array Class fill map repeat and join function

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,5 @@
-console.log(
-  function fizzbuzz(n=1) {
-    switch (true) {
-      case !(n % 5 && n % 3) :
-        return "FizzBuzz"
-      case n % 5 === 0:
-        return "Buzz"
-      case !(n % 3) :
-        return "Fizz"
-      default:
-        return "Not Fizz nor Buzz"
-    }
-  }()
-);
-// [fizzbuzz] using swtich and return
+function generateShape(integer=8) {
+  return Array(integer).fill("+").map(()=>"+".repeat(integer)).join`\n`
+}
+console.log(generateShape());
+// [generateShape] using Array Class fill map repeat and join function
