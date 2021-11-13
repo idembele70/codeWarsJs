b0ce8c3cbb087cef05cac804f5932f8012002f2e
idembele70 <idembele70@gmail.com>
2021-11-13 23:07:43 +0100

    [generateShape] using Array Class from repeat and join function

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 function generateShape(integer=8) {
-  return Array(integer).fill("+").map(()=>"+".repeat(integer)).join`\n`
+  return Array.from({length : integer}, ()=>"+".repeat(integer)).join(`\n`)
 }
 console.log(generateShape());
 // [generateShape] using Array Class fill map repeat and join function
