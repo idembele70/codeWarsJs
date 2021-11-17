e7ebbff7ff826b85997a326dca6de7f5a261958a
idembele70 <idembele70@gmail.com>
2021-11-17 15:08:04 +0100

    [sortme] using sort function from Array classes

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,5 @@
-function decode(message = "r slkv mlylwb wvxlwvh gsrh nvhhztv") {
-  const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-  ]
-  return [...message].map(
-    x => {
-      if (x != " ") {
-        let replace= 25-alphabet.indexOf(x)
-        return alphabet.slice(replace++,replace)
-      }
-      return x
-    }
-  ).join('')
-}
-console.log(decode());
-// [decode] using let variable destructuration map indexOf slice and join functions
+console.log(function sortme(names=["one", "two", "three"]){
+return names.sort()
+}());
+// [sortme] using sort function from Array classes
+console.log("zer"["rez".indexOf("z")])
