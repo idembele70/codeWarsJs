e208bb029899dc21576fef6adf7cd0cc9d25ea18
idembele70 <idembele70@gmail.com>
2021-11-29 13:45:53 +0100

    [getDivisorsCnt] using map Arry classes filter and length functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
-function addFive(num=5) {
-  const total = num + 5
-  return total
+function getDivisorsCnt(n=4) {
+  return [...Array(n)].map((_,idx)=>++idx).filter(
+    (x)=>n % x === 0).length
 }
-// [addFive] using const variable and addition
+console.log(getDivisorsCnt());
+// [getDivisorsCnt] using map Arry classes filter and length functions
