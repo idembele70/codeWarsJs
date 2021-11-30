87be58ab85d425fa75492253721c4c027fc78e66
idembele70 <idembele70@gmail.com>
2021-11-30 21:51:36 +0100

    [duplicateCount] trying

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,10 @@
-function getDivisorsCnt(n=4) {
-  let length = 0
-  for (let i = 1; i <= n; i++) {
-    if(n%i === 0) length++ 
-  }
-  return length
-}
-console.log(getDivisorsCnt());
-// [getDivisorsCnt] using let variable concactenation and for iteration
+console.log(
+  function duplicateCount(text="aabbcde") {
+  let duplicateCounter= []
+    for (const letter of text) {
+      if(!duplicateCounter.includes(letter.toLowerCase()) && text.indexOf(letter.toLowerCase()) != text.lastIndexOf(letter.toLowerCase()))duplicateCounter.push(letter)
+    }
+    return duplicateCounter.length
+  }()
+);
+// [duplicateCount] trying
