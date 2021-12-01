dc171e91ff5052ba91fa940c73895eb9d935bcc9
idembele70 <idembele70@gmail.com>
2021-12-01 16:49:03 +0100

    [duplicateCount] using let variable, toLocaleLowerCase forof iteration match RegExp includes push

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,14 @@
 console.log(
-  function duplicateCount(text="aabbcde") {
+  function duplicateCount(text="Indivisibility") {
   let duplicateCounter= []
+  text = text.toLocaleLowerCase()
     for (const letter of text) {
-      if(!duplicateCounter.includes(letter.toLowerCase()) && text.indexOf(letter.toLowerCase()) != text.lastIndexOf(letter.toLowerCase()))duplicateCounter.push(letter)
+      if(text.match(new RegExp(letter,"ig")).length > 1 && !duplicateCounter.includes(letter)){
+        duplicateCounter.push(letter)
+        console.log(letter)
+      }
     }
     return duplicateCounter.length
   }()
 );
-// [duplicateCount] trying
+// [duplicateCount] using let variable, toLocaleLowerCase forof iteration match RegExp includes push and log function and length
