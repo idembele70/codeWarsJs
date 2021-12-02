21cb5fedb14094c72d5f81c18c0d6ba8a7fcc0c9
idembele70 <idembele70@gmail.com>
2021-12-02 22:00:16 +0100

    [duplicateCount] using filter function

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,6 @@
-console.log(
-  function duplicateCount(text="Indivisibility") {
-  let duplicateCounter= []
-  text = text.toLocaleLowerCase()
-    for (const letter of text) {
-      if(text.match(new RegExp(letter,"ig")).length > 1 && !duplicateCounter.includes(letter)){
-        duplicateCounter.push(letter)
-        console.log(letter)
-      }
-    }
-    return duplicateCounter.length
-  }()
-);
-// [duplicateCount] using let variable, toLocaleLowerCase forof iteration match RegExp includes push and log function and length
+function getEvenNumbers(numbersArray=[1,2,3,6,8,10]) {
+  return numbersArray.filter(
+    number=>number%2==0
+  )
+}
+// [duplicateCount] using filter function
