b817f91e13924c6bc8a06ee859033d71b312f6e5
idembele70 <idembele70@gmail.com>
2021-12-06 00:49:42 +0100

    [removeSmallest] trying

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,13 @@
-console.log(
-  function solution(string="camelCasing") {
-    newString = ""
-    for (let i = 0; i < string.length; i++) {
-      const letter = string[i];
-      if(/[A-Z]/.test(letter))
-      newString +=` ${letter}`
-      else
-      newString+= letter
+function removeSmallest(numbers = [1, 2, 3, 4, 5, 1]) {
+  const min = Math.min(...numbers)
+  for (const number of numbers) {
+    if(min === number){
+      numbers.splice(numbers.indexOf(number),1)
+      break;
     }
-    return newString
-  }()
-);
-// [solution] using for iteration if condition and regex test
+    
+  }
+  return numbers
+}
+console.log(removeSmallest());
+// [removeSmallest] trying
