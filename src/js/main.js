20e93dbb53172131bbac7b52764221a57fd7ba96
idembele70 <idembele70@gmail.com>
2021-12-11 16:23:05 +0100

    [getSum] done

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,18 @@
-function getSum(a = -1, b = 2) {
-  return Array.from({ length: a + b + 1 }, (v, i) => a + i)/*  .reduce(
-    (acc, cur) => acc + cur, 0
-  )*/
-}
-console.log(getSum());
-// [getSum] trying
+console.log(
+  function greek_comparator(lhs, rhs) {
+    const greek_alphabet = ['alpha', 'beta', 'gamma', 'delta', 'epsilon', 'zeta',
+      'eta', 'theta', 'iota', 'kappa', 'lambda', 'mu',
+      'nu', 'xi', 'omicron', 'pi', 'rho', 'sigma',
+      'tau', 'upsilon', 'phi', 'chi', 'psi', 'omega']
+    const lhsIndex = greek_alphabet.indexOf(lhs)
+    const rhsIndex = greek_alphabet.indexOf(rhs)
+    if (lhsIndex - rhsIndex < 0) {
+      return "result should be negative"
+    } else if (lhsIndex - rhsIndex === 0) {
+      return 0
+    } else {
+      return "result should be positive"
+    }
+  }()
+);
+// [getSum] done
