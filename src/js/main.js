124bf48b58d60746c5d8aa05740e05858e55d9b7
idembele70 <idembele70@gmail.com>
2021-12-15 23:36:08 +0100

    [tocamelCase] using const variable boolean forof iteration if condition push uppercase and join func

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,16 @@
 function toCamelCase(str="the_stealth_warrior") {
-  return str.replace(/_./ig,v=>v[1].toUpperCase())
+  const strToCamelCase = []
+  canUpToUpper = false
+  for (const letter of str) {
+    if(letter === "_")
+    canUpToUpper = true
+    else if(canUpToUpper){
+      strToCamelCase.push(letter.toUpperCase())
+      canUpToUpper = false
+    } else
+    strToCamelCase.push(letter)
+  }
+  return strToCamelCase.join('')
 }
 console.log(toCamelCase());
-// [tocamelCase] using replace and uppercase functions
+// [tocamelCase] using const variable boolean forof iteration if condition push uppercase and join function 
