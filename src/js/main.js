9de01319913e2c1efaeb46723d5c2697070b96d0
idembele70 <idembele70@gmail.com>
2021-12-16 23:10:49 +0100

    [sumTwoSmallestNumbers] using sort function

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,9 @@
-function toCamelCase(str="the_stealth_warrior") {
-  const strToCamelCase = []
-  canUpToUpper = false
-  for (const letter of str) {
-    if(letter === "_")
-    canUpToUpper = true
-    else if(canUpToUpper){
-      strToCamelCase.push(letter.toUpperCase())
-      canUpToUpper = false
-    } else
-    strToCamelCase.push(letter)
-  }
-  return strToCamelCase.join('')
-}
-console.log(toCamelCase());
-// [tocamelCase] using const variable boolean forof iteration if condition push uppercase and join function 
+console.log(
+  function sumTwoSmallestNumbers(numbers = [5, 8, 12, 19, 22]) {
+    numbers.sort((a,b)=>a-b)
+    const minOne = numbers[0]
+    const minTwo = numbers[1]
+    return minOne+minTwo
+  }()
+);
+// [sumTwoSmallestNumbers] using sort function
