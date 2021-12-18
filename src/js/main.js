7ec327f67c1b6b3c97ba6504eb39fe7bdd40fb22
idembele70 <idembele70@gmail.com>
2021-12-18 15:27:30 +0100

    [isSquare] using every sqrt match

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,5 @@
-console.log(
-  function sumTwoSmallestNumbers(numbers = [15, 28, 4, 2, 43]) {
-    const mins = []
-    numbers.forEach(
-      x => {
-        if (mins.length < 2) mins.push(x)
-        else if (mins[0] > x) {
-          mins.splice(0, 1)
-          mins.push(x)
-        }
-        else if (mins[1] > x) {
-          mins.splice(1, 1)
-          mins.push(x)
-        }
-      }
-    )
-    const min = Math.min(...mins)
-    const max = Math.max(...mins)
-    console.log(mins)
-    return max + min
-  }()
-);
-// [sumTwoSmallestNumbers] trying
+function isSquare(arr = [2, 4, 9, 16]) {
+  return arr.every(x => Math.sqrt(x) + "".match(/./ig))
+}
+console.log(isSquare());
+// [isSquare] using every sqrt match
