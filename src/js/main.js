95bcd1c4f3f41e564bd48122bf1e717704b706f4
idembele70 <idembele70@gmail.com>
2021-12-23 16:54:39 +0100

    [median] using sort destructuring object

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
-function isSquare(arr = []) {
-  return arr.map(
-    number=>Math.sqrt(number).toString().includes('.')
-  ).includes(true)
-}
-console.log(isSquare());
-// [isSquare] using trying
+console.log(
+  function median(array = [33, 99, 100, 30, 29, 50]) {
+    array = array.sort((a, b) => a - b)
+    const { length } = array
+    return length % 2 !== 0 ? array[(length -1) / 2] : (array[length / 2] + array[length / 2 -1])/2
+  }()
+);
+// [median] using sort destructuring object
