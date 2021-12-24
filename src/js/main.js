92b802dcee521154cee963477eae471068f4cf85
idembele70 <idembele70@gmail.com>
2021-12-24 23:10:13 +0100

    median] using sort destructuring object

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 console.log(
   function median(array = [33, 99, 100, 30, 29, 50]) {
-    array = array.sort((a, b) => a - b)
+    array = array.sort((a, b) => a -b)
     const { length } = array
     return length % 2 !== 0 ? array[(length -1) / 2] : (array[length / 2] + array[length / 2 -1])/2
   }()
