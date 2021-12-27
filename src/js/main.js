a4af3569c42ba9426c3c848edc53b72399819aed
idembele70 <idembele70@gmail.com>
2021-12-27 18:31:14 +0100

    [codinggame exo] done using new Array classes fill map indexOf and console.log

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,16 @@
-const arr = [1, 2, 4]
+ const arr = [1, 2, 4]
 const arr2 = [2, 4, 3, 5]
 const m = (n) => n+1
-const n = m(arr2.length)
+const n = m(arr.length)
 const array = new Array(n).fill(1).map(
   (x,idx)=>x+idx
 )
 for (let i = 0; i < array.length; i++) {
   const element = array[i];
-  console.log(arr[element])
+  if(arr.indexOf(element) === -1){
+  console.log(element)
+  break;
+  }
 }
 
-// [codinggame exo] trying
+// [codinggame exo] done using new Array classes fill map indexOf and console.log
