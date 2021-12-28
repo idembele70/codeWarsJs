40ab571d0bfcf1c52fb5da9038db59bc7f3022c0
idembele70 <idembele70@gmail.com>
2021-12-28 22:39:18 +0100

    [isIsogram] trying

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,9 @@
- const arr = [1, 2, 4]
-const arr2 = [2, 4, 3, 5]
-const m = (n) => n+1
-const n = m(arr.length)
-const array = new Array(n).fill(1).map(
-  (x,idx)=>x+idx
-)
-for (let i = 0; i < array.length; i++) {
-  const element = array[i];
-  if(arr.indexOf(element) === -1){
-  console.log(element)
-  break;
-  }
-}
+console.log(
+  function isIsogram(str="Dermatoglyphics") {
+  return [...str].every(
+     (letter,_,arr)=>arr.indexOf(letter) === arr.lastIndexOf(letter)
+   )
+  }()
+);
 
-// [codinggame exo] done using new Array classes fill map indexOf and console.log
+// [isIsogram] trying
