ac029eb6f0848a45f156f3c2f6eed845a549b77b
idembele70 <idembele70@gmail.com>
2022-01-04 14:36:28 +0100

    [descenginOrder] using toString sort join and + operator to make string number

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,9 @@
-function filter_list(l=[1,2,"O","123"]) {
-  const filteredList = []
-  l.forEach(element => {
-    if(typeof element !== "string")
-    filteredList.push(element)
-  });
-  return filteredList
-}
-console.log(filter_list());
+console.log(
+  function descenginOrder(n = 1234) {
+    return +[...n.toString()].sort(
+      (a, b) => b - a
+    ).join('')
+  }()
+);
 
+// [descenginOrder] using toString sort join and + operator to make string number
