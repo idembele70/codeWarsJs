e651b6f8c57fccb3a7707b253c575c1f952b71bf
idembele70 <idembele70@gmail.com>
2022-02-01 15:41:29 +0100

    [greetDevelopers] using map iteration and template string

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,11 @@
-console.log(
-  function sortByBit(array=[30,0]) {
-    return array.map(el=>2**el).reduceRight(
-      (a,c)=>a+=c,0
-    )
-  }()
-);
-
-
-// [sortByBit] using map iteration and reduceRight map
+const lists = [
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+]
+function greetDevelopers(list=lists) {
+  return list.map(
+    (item)=>({...item,greeting:`Hi ${item.firstName}, what do you like the most about ${item.language}?`})
+  )
+}
+// [greetDevelopers] using map iteration and template string
