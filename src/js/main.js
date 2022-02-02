25747295957abe0035c512edd8c32e6153a84481
idembele70 <idembele70@gmail.com>
2022-02-02 11:21:32 +0100

    [countLanguages] using const object forof iteration if else statement

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,18 @@
-const lists = [
-  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
-  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
-  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
-]
-function greetDevelopers(list=lists) {
-  return list.map(
-    (item)=>({...item,greeting:`Hi ${item.firstName}, what do you like the most about ${item.language}?`})
-  )
-}
-// [greetDevelopers] using map iteration and template string
+var list1 = [
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C' },
+  { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript' },
+  { firstName: 'Ramon', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby' },
+  { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C' },
+];
+console.log(function countLanguages(list=list1) {
+  const counter = {}
+  for (const listItem of list) {
+    const { language } = listItem;
+    if(counter[language])
+    counter[language] +=1
+    else
+    counter[language] = 1
+  }
+  return counter
+}())
+// [countLanguages] using const object forof iteration if else statement
