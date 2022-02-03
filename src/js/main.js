40944df8c94571463be47a11a152e3a2a702bcac
idembele70 <idembele70@gmail.com>
2022-02-03 14:27:28 +0100

    [digitize] using tostring and Number class

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,5 @@
-var list1 = [
-  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C' },
-  { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript' },
-  { firstName: 'Ramon', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby' },
-  { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C' },
-];
-console.log(function countLanguages(list = list1) {
-  return list.reduce((previous, {language}) => {   
-    if (previous[language]){
-       previous[language] += 1
-    }
-    else{
-      previous[language] = 1
-    }
-    return previous
-  }, {})
-}())
-// [countLanguages] using reduce function from array async
+function digitize(n=123) {
+  return [...n.toString()].map(Number)
+}
+console.log(digitize());
+// [digitize] using tostring and Number class
