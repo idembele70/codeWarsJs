7ecb6b599402842f048cc439ad993e0fa98e3c74
idembele70 <idembele70@gmail.com>
2022-02-17 11:37:07 +0100

    [helloworld] trying

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
-console.log(
-  (function numberToString(num=78) {
-    return num+"" 
-  })()
-)
-// [numberToString] using string addition
+const helloWorld = () => {
+  const tabs = [72, 101, 108, 108, 111,
+    44,  32,  87, 111, 114,
+   108, 100,33]
+  return tabs.map(x=>String.fromCharCode(x)).join('')
+}
+console.log(helloWorld())
+// [helloworld] trying
