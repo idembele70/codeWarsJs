7b6f922ef3fca8403595d38838d7958075c047e1
idembele70 <idembele70@gmail.com>
2022-02-18 12:01:50 +0100

    [getCount] using match and or assignment and array length

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
-const helloWorld = () => {
-  const tabs = [72, 101, 108, 108, 111,
-    44,  32,  87, 111, 114,
-   108, 100,33]
-  return tabs.map(x=>String.fromCharCode(x)).join('')
-}
-console.log(helloWorld())
-// [helloworld] trying
+console.log(
+  function getCount(str="abracadabra") {
+  const matchArr = str.match(/[aeiou]/ig) || []
+  return matchArr.length
+  }()
+)
+
+// [getCount] using match and or assignment and array length
