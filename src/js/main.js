6c76c3a864ac88c1e08b671854e84c6ee4a985a7
idembele70 <idembele70@gmail.com>
2022-02-21 17:09:32 +0100

    [isVeryEvenNumber] using WHILE iteration and return

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
-console.log(
-  function getCount(str="abracadabra") {
-  return str.replace(/[^aeiou]/ig, "").length
-  }()
-)
+const isVeryEvenNumber = (n = 5) => {
+  while (n.toString().length > 1) {
+    n = [...n.toString()].reduce(
+      (acc, cur) => acc + +cur, 0
+    )
+  }
+  return +n % 2 === 0
+}
+console.log(isVeryEvenNumber())
 
-// [getCount] using regex replace
+// 
