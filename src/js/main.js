2d9d41995208056689c7b10adfb84cac206ca59e
idembele70 <idembele70@gmail.com>
2022-03-06 02:01:24 +0100

    [getDivisorsCnt] using filter and forin iteration Number isInteger

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 function getEvenNumbers(numbersArray=[ 0,28,15,83,85,27,81,74,50]) {
   for (const index in numbersArray) {
     if(numbersArray[index] % 2)
-    numbersArray.splice(index,1,undefined)
+    numbersArray.splice(index,1,null)
   }
-  return numbersArray.filter(x=>x !== undefined)
+  return numbersArray.filter(x=>Number.isInteger(x))
 }
 console.log(getEvenNumbers())
-// [getDivisorsCnt] using filter and forin iteration
+// [getDivisorsCnt] using filter and forin iteration Number isInteger
