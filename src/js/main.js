aaf46f73407d3f84b30c9a46cd496a3a1246e981
idembele70 <idembele70@gmail.com>
2022-03-10 23:57:45 +0100

    [add] trying

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
-function solution(string="camelCasing") {
-  return [...string].map(
-    x=>x.replace(/([A-Z])/, " $1")
-  ).join('')
-}
-console.log(solution())
-// [solution] using destructuring map regex replace and join
+console.log(
+  function add(n) {
+    return v=>v+n
+  }()
+)
+// [add] trying
