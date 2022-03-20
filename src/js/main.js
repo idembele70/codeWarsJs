29ea64be7d760239294c414d78f651d7687e0fe4
idembele70 <idembele70@gmail.com>
2022-03-20 23:30:37 +0100

    [sortme] using sort

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,2 @@
-console.log(
-  function friend(friends=["Ryan", "Kieran", "Mark"]) {
-    const newfriends = []
-    for (const friend of friends) {
-      if(friend.length === 4)
-      newfriends.push(friend)
-    }
-    return newfriends
-  }()
-)
-// [friend] using for of iteration and array push functions
+const sortme = (names) => names.sort((a,b)=>a-b)
+// [sortme] using sort
