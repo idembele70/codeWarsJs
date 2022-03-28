ffa5c70d75aa917fbe07bab707ba0805884f0074
idembele70 <idembele70@gmail.com>
2022-03-28 13:25:16 +0200

    [filterLongWord] using split, template string in split and filter functions, destructurind, and in conditions

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,5 @@
-function filterLongWord(sentence, n) {
-  return sentence.split` `.filter(
-    ({ length }) => length > n
-  )
+function palindrome(num="-451") {
+  return typeof num === "number" && num >=0 ?parseInt(`${num}`.split("").reverse().join("")) === num  :"Not valid"
 }
-console.log(filterLongWord())
-// [filterLongWord] using split, template string in split and filter functions, destructuring
+console.log(palindrome())
+// [filterLongWord] using split, template string in split and filter functions, destructurind, and in conditions
