adf767243010e159ae7e8243172ad118078b15a0
idembele70 <idembele70@gmail.com>
2022-04-03 23:59:33 +0200

    [sortArray] trying

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 console.log(
   function sortArray(array=[5, 3, 2, 8, 1, 4]) {
+    const arr = []
    return array.sort((a,b)=> a % 2 === 0 ?a : a - b)
-
   }()
 );
-// [getAge] trying
+// [sortArray] trying
